897502c469c0140bf2d9ef7c1deeba0acb28e476
hetk987 <hetuniverse@gmail.com>
2024-04-15 10:10:46 -0500

    Added background animation of changing gradient and a moving wave at the bottom

--- leaderboard/src/app/page.tsx
+++ leaderboard/src/app/page.tsx
@@ -1,12 +1,16 @@
 /* page.tsx */
+"use client";
+import { useEffect, useRef, useState } from 'react';
 import './styles.css';
 import Link from "next/link";
+
 
 import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
-export default async function Home() {
+export default function Home() {  
+
   const competitors = [
     { name: 'Competitor 1', score: 100 },
     { name: 'Competitor 2', score: 90 },
@@ -15,14 +19,14 @@ export default async function Home() {
   const maxScore = Math.max(...competitors.map(c => c.score));
   return (
     <>
-      <h1 className="text-3xl font-bold mb-4 text-center">Leaderboard</h1>
+      <h1 className="text-5xl font-bold mb-4 text-center">Leaderboard</h1>
       <div className="p-6 w-1/2 mx-auto bg-white rounded-xl shadow-md flex flex-col items-center space-y-4">
         <ul className="list-decimal list-inside w-full">
           {competitors.map((competitor, index) => (
-            <li key={index} className="p-4 bg-blue-100 rounded-lg mb-4 animate-width" style={{ animationName: `loading-${index}`}}>
+            <li key={index} className="p-4 bg-blue-100 rounded-lg mb-4 animate-width" style={{ animationName: `loading-${index}`, backgroundColor: `rgb(51, ${92 + 50 * index}, 255)`}}>
               <style>
                 {`
-                  @keyframes loading-${index} {
+                    @keyframes loading-${index} {
                     0% {
                       width: 150px;
                     }
@@ -36,8 +40,21 @@ export default async function Home() {
               <p className="mt-2">Score: {competitor.score}</p>
             </li>
           ))}
-        </ul>
-      </div>
+        </ul> 
+        
+    </div>
+    <div></div>
+    <svg className="waves"  viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+<defs>
+<path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+</defs>
+<g className="parallax">
+<use href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+<use href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+<use href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+<use href="#gentle-wave" x="48" y="7" fill="#fff" />
+</g>
+</svg>
     </>
   );
 }
