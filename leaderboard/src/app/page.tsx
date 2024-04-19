16fac808b4028a6a4c2b991ad480fc6a6d269c9e
Dayo Bamgbelu <ddmds66@gmail.com>
2024-04-19 16:19:06 -0500

    added (hopefully!) working functionality

--- leaderboard/src/app/page.tsx
+++ leaderboard/src/app/page.tsx
@@ -2,16 +2,33 @@
 "use client";
 import './styles.css';
 import Link from "next/link";
+import { useState, useEffect} from 'react';
 
+interface Team{
+  teamId: number;
+  teamName: string;
+  points: number;
+  rank: number;
+}
 
 export default function Home() {  
 
-  const competitors = [
+  /*const competitors = [
     { name: 'Competitor 1', score: 100 },
     { name: 'Competitor 2', score: 90 },
     { name: 'Competitor 3', score: 80 },
-  ];
-  const maxScore = Math.max(...competitors.map(c => c.score));
+  ]; */
+
+  const [topThree, setTopThree] = useState<Team[]>([]);
+
+  useEffect(() => {
+    //leaderboard fetching
+    fetch('../api/routers/team/getTopThree')
+      .then(response => response.json())
+      .then(data => setTopThree(data.topThree))
+      .catch(error => console.error('Error fetching top three:', error));
+  }, []);
+  const maxScore = Math.max(...topThree.map(c => c.score));
   return (
     <>
       <div className="absolute top-0 right-0 m-4">
@@ -22,7 +39,7 @@ export default function Home() {
       <h1 className="text-5xl font-bold mb-4 text-center">Leaderboard</h1>
       <div className="p-6 w-1/2 mx-auto bg-white rounded-xl shadow-md flex flex-col items-center space-y-4">
         <ul className="list-decimal list-inside w-full">
-          {competitors.map((competitor, index) => (
+          {topThree.map((team, index) => (
             <li key={index} className="p-4 bg-blue-100 rounded-lg mb-4 animate-width" style={{ animationName: `loading-${index}`, backgroundColor: `rgb(51, ${92 + 50 * index}, 255)`}}>
               <style>
                 {`
@@ -31,13 +48,13 @@ export default function Home() {
                       width: 150px;
                     }
                     100% {
-                      width: ${(competitor.score / maxScore) * 100}%;
+                      width: ${(team.points / maxScore) * 100}%;
                     }
                   }
                 `}
               </style>
-              <h2 className="text-lg font-semibold">{competitor.name}</h2>
-              <p className="mt-2">Score: {competitor.score}</p>
+              <h2 className="text-lg font-semibold">{team.teamName}</h2>
+              <p className="mt-2">Score: {team.points}</p>
             </li>
           ))}
         </ul> 
--- leaderboard/src/app/relativeLeaderboard/page.tsx
+++ leaderboard/src/app/relativeLeaderboard/page.tsx
@@ -1,50 +1,108 @@
 "use client";
+import { useQuery } from '@tramvai/react-query';
+import { postRouter } from '~/server/api/routers/team';
+
+interface Team {
+  id: number;
+  name: string;
+  score: number;
+  rank: number;
+}
+
+interface ScoreDiffData {
+  teamAhead?: Team;
+  curTeam?: Team;
+  teamBehind?: Team;
+}
 
 export default function Info() {
-    const competitors = [
-        { name: 'Competitor 1', score: 100 },
-        { name: 'Competitor 2', score: 90 },
-        { name: 'Competitor 3', score: 80 },
-      ];
-      const maxScore = Math.max(...competitors.map(c => c.score));
-      return (
-        <>
-          <h1 className="text-5xl font-bold mb-4 text-center">Leaderboard</h1>
-          <div className="p-6 w-1/2 mx-auto bg-white rounded-xl shadow-md flex flex-col items-center space-y-4">
-            <ul className="list-decimal list-inside w-full">
-              {competitors.map((competitor, index) => (
-                <li key={index} className="p-4 bg-blue-100 rounded-lg mb-4 animate-width" style={{ animationName: `loading-${index}`, backgroundColor: `rgb(51, ${92 + 50 * index}, 255)`}}>
-                  <style>
-                    {`
-                        @keyframes loading-${index} {
-                        0% {
-                          width: 150px;
-                        }
-                        100% {
-                          width: ${(competitor.score / maxScore) * 100}%;
-                        }
-                      }
-                    `}
-                  </style>
-                  <h2 className="text-lg font-semibold">{competitor.name}</h2>
-                  <p className="mt-2">Score: {competitor.score}</p>
-                </li>
-              ))}
-            </ul> 
-            
-        </div>
-        <div></div>
-        <svg className="waves"  viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
-    <defs>
-    <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-    </defs>
-    <g className="parallax">
-    <use href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
-    <use href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
-    <use href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
-    <use href="#gentle-wave" x="48" y="7" fill="#fff" />
-    </g>
-    </svg>
-        </>
-      );
+
+  /*
+  const competitors = [
+    { name: 'Competitor 1', score: 110, rank: 3 },
+    { name: 'Competitor 2', score: 91, rank: 4 },
+    { name: 'Competitor 3', score: 60, rank: 5 },
+  ]; 
+
+  // Assuming the current user is Competitor 2
+  const currentUser = competitors.find(c => c.name === 'Competitor 2');
+  const currentUserIndex = competitors.indexOf(currentUser!);
+  const personBehind = competitors[currentUserIndex + 1];
+  const personAhead = competitors[currentUserIndex - 1];
+
+  const pointsBehind = personBehind ? currentUser.score - personBehind.score : 0;
+  const pointsAhead = personAhead ? personAhead.score - currentUser.score : 0;
+*/
+  const { data: scoreDiff, isLoading, isError } = useQuery<ScoreDiffData>('scoreDiff', () =>
+    postRouter.scoreDiff.query()
+  );
+
+
+  const { teamAhead, curTeam, teamBehind } = scoreDiff || {};
+
+
+
+  //Checks to see if their rank is in top 10;
+  const ifTop10 = curTeam.rank <= 10;
+
+ /* const maxPointDifference = pointsAhead + pointsBehind; // Maximum point difference
+  const buffer = 10; // 10% buffer on each side
+
+  const getPosition = (scoreDifference: number, isCurrentUser: boolean, isAhead: boolean) => {
+    if (isCurrentUser) {
+      return '50%'; // Current user is always at the center
+    } else {
+      const positionPercentage = (scoreDifference / maxPointDifference) * (40);
+      console.log(positionPercentage);
+      if (positionPercentage > 40) {
+        return isAhead ? `${90}%` : `${10}%`;
+      }
+      return isAhead ? `${50 + positionPercentage}%` : `${50 - positionPercentage}%`;
     }
+  };
+*/
+  return (
+    <>
+      <h1 className="text-5xl font-bold mb-4 text-center text-blue-600">Leaderboard</h1>
+      <div className="w-full relative">
+        {ifTop10 && (
+          <div><h1 className="text-5xl font-bold mb-4 text-center text-blue-600">Rank: {currentUser.rank}</h1></div>
+        )}
+      </div>
+      <div className="p-10 h-1/2 rounded-xl flex flex-col items-center justify-center space-y-4">
+        <div className="flex justify-center space-x-4 items-center align-center align-top">
+          <div className="w-full relative flex justify-between gap-10">
+            {teamBehind && (
+              <div className="square-outline flex flex-col justify-center items-center bg-red-500 opacity-80">
+                -{teamBehind.score}
+              </div>
+            )}
+            {curTeam && (
+              <div className="square-outline flex flex-col justify-center items-center bg-white opacity-80">
+                {curTeam.score}
+              </div>
+            )}
+            {teamAhead && (
+              <div className="square-outline flex flex-col justify-center items-center bg-green-500 opacity-80">
+                +{teamAhead.score}
+              </div>
+            )}
+          </div>
+
+
+        </div>
+      </div>
+      <svg className="waves" viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
+        <defs>
+          <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+        </defs>
+        <g className="parallax">
+          <use href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+          <use href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+          <use href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+          <use href="#gentle-wave" x="48" y="7" fill="#fff" />
+        </g>
+      </svg>
+    </>
+  );
+}
