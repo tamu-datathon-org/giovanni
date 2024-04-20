d5f12f2c75d113721a5897dbed8629088364a3ce
Redninjale <redninjale@gmail.com>
2024-04-20 08:30:57 -0500

    Updating envjs

--- leaderboard/src/env.js
+++ leaderboard/src/env.js
@@ -13,6 +13,7 @@ export const env = createEnv({
       .default("development"),
     // DISCORD_CLIENT_ID: z.string(),
     // DISCORD_CLIENT_SECRET: z.string(),
+    CTFD_API_KEY: z.string(),
   },
 
   /**
@@ -35,6 +36,7 @@ export const env = createEnv({
     // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     // DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    CTFD_API_KEY: process.env.CTFD_API_KEY,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
