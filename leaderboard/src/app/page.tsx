b9b96ad2e71d0ee7ef6450a9829e23aa853d179f
Redninjale <redninjale@gmail.com>
2024-04-20 13:10:41 -0500

    Backend working

--- leaderboard/src/app/page.tsx
+++ leaderboard/src/app/page.tsx
@@ -1,9 +1,9 @@
 /* page.tsx */
-"use client";
 import "./styles.css";
 import Link from "next/link";
 import { useState } from "react";
-import { api } from "~/trpc/react";
+import { api } from "~/trpc/server";
+import { AdjacentTeamsComponent, TopThreeComponent } from "./components/TopThreeComponent";
 import { type Team } from "~/server/api/routers/team"; // interface Team {
 
 // interface Team {
@@ -13,7 +13,7 @@ import { type Team } from "~/server/api/routers/team"; // interface Team {
 //   rank: number;
 // }
 
-export default function Home() {
+export default async function Home() {
   /*const competitors = [
                     { name: 'Competitor 1', score: 100 },
                     { name: 'Competitor 2', score: 90 },
@@ -21,15 +21,15 @@ export default function Home() {
                 
                   ]; */
 
-  const [topThree, setTopThree] = useState<Team[]>([]);
+  // const [topThree, setTopThree] = useState<Team[]>([]);
+  const topThree = await api.team.getTopThree()
 
   //leaderboard fetching
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  setTopThree(api.team.getTopThree.useQuery());
+  // setTopThree(api.team.getTopThree.useQuery());
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  const maxScore = Math.max(...topThree.map((c) => c.score));
+  // const maxScore = Math.max(...topThree.map((c) => c.score));
 
   return (
     <>
@@ -51,77 +51,37 @@ export default function Home() {
       <h1 className="mb-4 text-center text-5xl font-bold">Leaderboard</h1>
       <div className="mx-auto flex w-1/2 flex-col items-center space-y-4 rounded-xl bg-white p-6 shadow-md">
         <ul className="w-full list-inside list-decimal">
-          {topThree.map((team, index) => (
-            <li
+          {topThree.map((team: Team, index: number) => (
+            <TopThreeComponent
               key={index}
-              className="animate-width mb-4 rounded-lg bg-blue-100 p-4"
-              style={{
-                animationName: `loading-${index}`,
-                backgroundColor: `rgb(51, ${92 + 50 * index}, 255)`,
-              }}
-            >
-              <style>
-                {`
-                @keyframes loading-${index} {
-                0% {
-                  width: 150px;
-                }
-                100% {
-                  width: ${(team.score / maxScore) * 100}%;
-                }
-              }
-            `}
-              </style>
-              <h2 className="text-lg font-semibold">{team.name}</h2>
-              <p className="mt-2">Score: {team.score}</p>
-            </li>
+              index={index}
+              team={team}
+              maxScore={100}
+            />
           ))}
         </ul>
       </div>
 
       <div className="mx-auto flex w-1/2 flex-col items-center space-y-4 rounded-xl bg-white p-6 opacity-95 shadow-md">
         <ul className="items-left flex w-full list-inside list-none flex-col">
-          {topThree.map((competitor, index) => {
-            const colors = [
+          {topThree.map((competitor: Team, index: number) => {
+            const colors: string[] = [
               "bg-yellow-300",
               "bg-gray-400",
               "bg-orange-500",
               "bg-yellow-500",
               "bg-purple-500",
             ];
-            const colorIndex = index % colors.length; // Get the color index from the palette
-            const colorClass = colors[colorIndex]; // Get the color class
+            const colorIndex: number = index % colors.length; // Get the color index from the palette
+            const colorClass: string | undefined = colors[colorIndex]; // Get the color class
 
             return (
-              <li
+              <AdjacentTeamsComponent
                 key={index}
-                className="animate-width relative mb-6 rounded-lg bg-blue-100 p-8"
-                style={{
-                  animationName: `loading-${index}`,
-                  backgroundColor: `rgb(51, ${92 + 50 * index}, 255)`,
-                }}
-              >
-                <style>
-                  {`
-                @keyframes loading-${index} {
-                  0% {
-                    width: 150px;
-                  }
-                  100% {
-                    width: ${100 / (index + 1)}%;
-                  }
-                }
-              `}
-                </style>
-                <h2 className="absolute inset-0 flex items-center justify-center text-2xl font-semibold text-white">
-                  {competitor.name}
-                </h2>
-                <div
-                  className={`absolute bottom-0 right-0 top-0 flex h-full w-8 items-center justify-center rounded-lg ${colorClass} z-0`}
-                >
-                  <span className="text-lg text-white">{index + 1}</span>
-                </div>
-              </li>
+                index={index}
+                competitor={competitor}
+                colorClass={colorClass}
+              />
             );
           })}
         </ul>
