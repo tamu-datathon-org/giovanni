bcdaf14c235390b6dcd2aba7ebbe1ecc7e6a4289
Betheony <bethany@code2college.org>
2024-06-24 19:12:32 -0500

    markdown added

--- my-turborepo/apps/emails/index.tsx
+++ my-turborepo/apps/emails/index.tsx
@@ -1,9 +1,22 @@
 import { Button, Html } from "@react-email/components";
 import * as React from "react";
+import { Markdown } from "@react-email/components";
 
 export default function Email() {
   return (
     <Html>
+      <Markdown
+        markdownCustomStyles={{
+          h1: { color: "red" },
+          h2: { color: "blue" },
+          codeInline: { background: "grey" },
+        }}
+        markdownContainerStyles={{
+          padding: "12px",
+          border: "solid 1px black",
+        }}
+      >{`# Hello, World!`}</Markdown>
+
       <Button
         href="https://example.com"
         style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
