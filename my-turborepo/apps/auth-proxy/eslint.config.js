f2ed73c1f2a6d69f6e4e254e5ea7ab3074d26497
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-06-26 21:27:26 -0500

    Added some Auth0 stuff

--- my-turborepo/apps/auth-proxy/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import baseConfig from "@vanni/eslint-config/base";
-
-/** @type {import('typescript-eslint').Config} */
-export default [
-  {
-    ignores: [".nitro/**", ".output/**"],
-  },
-  ...baseConfig,
-];
