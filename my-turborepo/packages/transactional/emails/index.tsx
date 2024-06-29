8988396c30e83efff3c73f87b7149406d8ac6e2a
Betheony <bethany@code2college.org>
2024-06-29 13:30:30 -0500

    Updated index tsx w username

--- my-turborepo/packages/transactional/emails/index.tsx
+++ my-turborepo/packages/transactional/emails/index.tsx
@@ -1,28 +1,74 @@
-import { Button, Html } from "@react-email/components";
-import * as React from "react";
-import { Markdown } from "@react-email/components";
+import {
+  Body,
+  Button,
+  Column,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Row,
+  Section,
+  Tailwind,
+  Text,
+} from '@react-email/components';
+import { Markdown } from '@react-email/components';
+import * as React from 'react';
 
-export default function Email() {
+interface VercelInviteUserEmailProps {
+  username?: string;
+  userImage?: string;
+  invitedByUsername?: string;
+  invitedByEmail?: string;
+  teamName?: string;
+  teamImage?: string;
+  inviteLink?: string;
+  inviteFromIp?: string;
+  inviteFromLocation?: string;
+}
+
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : '';
+
+export const VercelInviteUserEmail = ({
+  username = 'zenorocha',
+  userImage = `${baseUrl}/static/vercel-user.png`,
+  invitedByUsername = 'bukinoshita',
+  invitedByEmail = 'bukinoshita@example.com',
+  teamName = 'My Project',
+  teamImage = `${baseUrl}/static/vercel-team.png`,
+  inviteLink = 'https://vercel.com/teams/invite/foo',
+  inviteFromIp = '204.13.186.218',
+  inviteFromLocation = 'São Paulo, Brazil',
+}: VercelInviteUserEmailProps) => {
+  const previewText = `Join ${invitedByUsername} on Vercel`;
+
   return (
     <Html>
-      <Markdown
-        markdownCustomStyles={{
-          h1: { color: "red" },
-          h2: { color: "blue" },
-          codeInline: { background: "grey" },
-        }}
-        markdownContainerStyles={{
-          padding: "12px",
-          border: "solid 1px black",
-        }}
-      >{`# Hello, World!`}</Markdown>
-
-      <Button
-        href="https://example.com"
-        style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
-      >
-        Click me
-      </Button>
+      <Head />
+      <Preview>{previewText}</Preview>
+      <Tailwind>
+        <Heading as="h2">Dear {username}</Heading>
+          <Body className="bg-white my-auto mx-auto font-sans">
+            <Markdown
+            markdownCustomStyles={{
+              h1: { color: "red" },
+              h2: { color: "blue" },
+              codeInline: { background: "grey" },
+            }}
+            markdownContainerStyles={{
+              padding: "12px",
+              border: "solid 1px black",
+            }}
+          >{`# Hello, World!`}</Markdown>
+          </Body>
+      </Tailwind>
     </Html>
   );
-}
+};
+
+export default VercelInviteUserEmail;
