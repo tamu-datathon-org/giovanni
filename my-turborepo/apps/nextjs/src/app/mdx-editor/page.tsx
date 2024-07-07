d453097d3b1cb5fc24b542faff488eb2999039d6
Betheony <bethany@code2college.org>
2024-07-07 15:41:26 -0500

    basic editor

--- my-turborepo/apps/nextjs/src/app/mdx-editor/page.tsx
+++ my-turborepo/apps/nextjs/src/app/mdx-editor/page.tsx
@@ -1,7 +1,23 @@
-export default function HomePage() {
+import dynamic from 'next/dynamic'
+import { Suspense } from 'react'
+
+const EditorComp = dynamic(() => import('../_components/EditorComponent'), { ssr: false })
+
+const markdown = `
+Hello **world**!
+`
+
+export default function Home() {
     return (
-        <div>
-            poo
-        </div>
-    );
+        <>
+            <p>This is a bare-bones unstyled MDX editor without any plugins and no toolbar. Check the EditorComponent.tsx file for the code.</p>
+            <p>To enable more features, add the respective plugins to your instance - see <a className="text-blue-600" href="https://mdxeditor.dev/editor/docs/getting-started">the docs</a> for more details.</p>
+            <br />
+            <div style={{border: '1px solid black'}}>
+                <Suspense fallback={null}>
+                    <EditorComp markdown={markdown} />
+                </Suspense>
+            </div>
+        </>
+    )
 }
