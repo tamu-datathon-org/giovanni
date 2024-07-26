6227dd0db90a6cb1ab624528db78a153a42356a8
Redninjale <redninjale@gmail.com>
2024-07-26 10:06:05 -0500

    build layout for testing

---
+++ my-turborepo/apps/nextjs/src/app/apply/actions.tsx
@@ -0,0 +1,13 @@
+// "use server";
+
+// export async function getFullName(data: FormData) {
+//   // we're gonna put a delay in here to simulate some kind of data processing like persisting data
+// //   await new Promise((resolve) => setTimeout(resolve, 1000));
+
+//     console.log("server action", data);
+    
+//     return {
+//         status: "success",
+//         message: `Welcome, ${data.get("firstName")} ${data.get("lastName")}!`,
+//     };
+// }
