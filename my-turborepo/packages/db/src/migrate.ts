a36b324e8fb0ce707522a7805867b9a2d166593c
redninjale <redninjale@gmail.com>
2024-08-12 17:20:47 -0500

    fix: formatting and linting fixes

--- my-turborepo/packages/db/src/migrate.ts
+++ my-turborepo/packages/db/src/migrate.ts
@@ -1,7 +1,8 @@
-import 'dotenv/config';
-import { migrate } from 'drizzle-orm/vercel-postgres/migrator';
-import { sql } from "@vercel/postgres";
-import { db } from './client';
+import "dotenv/config";
+
+import { migrate } from "drizzle-orm/vercel-postgres/migrator";
+
+import { db } from "./client";
 
 // This will run migrations on the database, skipping the ones already applied
-await migrate(db, { migrationsFolder: './drizzle' });
+await migrate(db, { migrationsFolder: "./drizzle" });
