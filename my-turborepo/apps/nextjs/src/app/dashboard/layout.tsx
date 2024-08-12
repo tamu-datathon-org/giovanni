4a761b4907cf15a1dec777f5c9a30be4cb6935a8
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-08-12 17:35:31 -0400

    WIP Linking

---
+++ my-turborepo/apps/nextjs/src/app/dashboard/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata, Viewport } from "next";
+import { SessionProvider } from "next-auth/react";
+
+import "~/app/globals.css";
+
+import { env } from "~/env";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(
+    env.VERCEL_ENV === "production"
+      ? "https://turbo.t3.gg"
+      : "http://localhost:3000",
+  ),
+  title: "Create T3 Turbo",
+  description: "Simple monorepo with shared backend for web & mobile apps",
+  openGraph: {
+    title: "Create T3 Turbo",
+    description: "Simple monorepo with shared backend for web & mobile apps",
+    url: "https://create-t3-turbo.vercel.app",
+    siteName: "Create T3 Turbo",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@jullerino",
+    creator: "@jullerino",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function AccountLayout(props: { children: React.ReactNode }) {
+  return <SessionProvider>{props.children}</SessionProvider>;
+}
