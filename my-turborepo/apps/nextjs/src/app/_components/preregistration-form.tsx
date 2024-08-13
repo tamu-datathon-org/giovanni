053e54bd4e86abc0ff52b35d56a4ecc63b8bb6eb
redninjale <redninjale@gmail.com>
2024-08-12 23:39:49 -0500

    feat: creating, deleting, and canceling emails included

--- my-turborepo/apps/nextjs/src/app/_components/preregistration-form.tsx
+++ my-turborepo/apps/nextjs/src/app/_components/preregistration-form.tsx
@@ -4,9 +4,11 @@ import { SubmitHandler, useForm } from "react-hook-form"
 import { preregistrationSchema, PreregistrationData } from "../preregistration/validation"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { api } from "~/trpc/react"
+import { FormEvent, FormEventHandler, MouseEventHandler, useState } from "react"
+import { z } from "zod"
 
-export const PreregistrationForm = () => {
-    const { handleSubmit, register, formState: { errors, isSubmitting, isDirty, isValid } } = useForm<PreregistrationData>({
+export const CreatePreregistrationForm = () => {
+    const { handleSubmit, register, formState: { errors, isSubmitting, isDirty } } = useForm<PreregistrationData>({
         resolver: zodResolver(preregistrationSchema)
     })
 
@@ -37,6 +39,55 @@ export const PreregistrationForm = () => {
                         <img src="loading.svg" className="animate-spin w-6 h-6" aria-hidden="true" alt="loading..." />
                         : "Submit"
                 }
+            </button>
+        </form>
+    )
+}
+
+export const DeletePreregistrationForm = () => {
+    interface FormData {
+        email: string;
+    }
+
+    const [formData, setFormData] = useState<FormData>({
+        email: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const deletePreregistration = api.preregistration.delete.useMutation();
+    const handleDelete: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.preventDefault();
+        deletePreregistration.mutate(formData.email);
+    }
+
+    const cancelPreregistration = api.preregistration.cancel.useMutation();
+    const handleCancel: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.preventDefault();
+        cancelPreregistration.mutate(formData)
+    }
+
+    return (
+        <form className="bg-gray-700 rounded flex flex-col justify-center items-center text-center w-1/2 text-lg">
+            <label>
+                <span>Enter Email:</span>
+                <input type="text" name="email" onChange={handleChange} />
+            </label>
+            <button
+                onClick={handleDelete}
+            >
+                Delete User
+            </button>
+            <button
+                onClick={handleCancel}
+            >
+                Cancel User
             </button>
         </form>
     )
