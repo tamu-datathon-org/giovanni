a5696571f64a1dd4a9c83853b1806132f1c71339
redninjale <redninjale@gmail.com>
2024-08-12 21:11:47 -0500

    feat: add email to preregistration box

--- my-turborepo/apps/nextjs/src/app/_components/preregistration-form.tsx
+++ my-turborepo/apps/nextjs/src/app/_components/preregistration-form.tsx
@@ -3,19 +3,35 @@
 import { SubmitHandler, useForm } from "react-hook-form"
 import { preregistrationSchema, PreregistrationData } from "../preregistration/validation"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { api } from "~/trpc/react"
 
 export const PreregistrationForm = () => {
-    const { handleSubmit, register } = useForm<PreregistrationData>({
+    const { handleSubmit, register, formState: { errors, isSubmitting, isDirty, isValid } } = useForm<PreregistrationData>({
         resolver: zodResolver(preregistrationSchema)
     })
 
-    const onSubmit: SubmitHandler<PreregistrationData> = data => console.log(data)
+    const createPreregistration = api.preregistration.create.useMutation();
+
+    const onSubmit: SubmitHandler<PreregistrationData> = async data => {
+        await createPreregistration.mutateAsync({ email: data.email })
+    }
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <input {...register("email", { required: true, maxLength: 256 })} />
-            <input {...register("confirmation", { required: true, })} />
-            <input type="submit" />
+            {errors?.email?.message}
+            <input type="checkbox" value={"on"} {...register("confirmation", { required: true, })} />
+            {errors?.confirmation?.message}
+            <button
+                type="submit"
+                disabled={!isDirty || isSubmitting}
+            >
+                {
+                    isSubmitting ?
+                        <img src="loading.svg" className="animate-spin w-6 h-6" aria-hidden="true" alt="loading..." />
+                        : "Submit"
+                }
+            </button>
         </form>
     )
 }
