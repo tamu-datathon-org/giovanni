f3e21fba653b1d205419d4121885d248cfa28e01
redninjale <redninjale@gmail.com>
2024-08-12 23:39:49 -0500

    feat: creating, deleting, and canceling emails included

--- my-turborepo/apps/nextjs/src/app/preregistration/page.tsx
+++ my-turborepo/apps/nextjs/src/app/preregistration/page.tsx
@@ -1,11 +1,13 @@
-import { SubmitHandler } from "react-hook-form";
-import { PreregistrationForm } from "../_components/preregistration-form";
+import { CreatePreregistrationForm, DeletePreregistrationForm } from "../_components/preregistration-form";
 import { PreregistrationData } from "./validation";
 import { api } from "~/trpc/react";
 
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 export default function Page() {
     return (
-        < PreregistrationForm />
+        <>
+            < CreatePreregistrationForm />
+            < DeletePreregistrationForm />
+        </>
     )
 }
