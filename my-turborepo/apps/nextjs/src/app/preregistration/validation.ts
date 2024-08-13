f8495538b7827f175a1caae7fed3cdaf7a5e56f2
redninjale <redninjale@gmail.com>
2024-08-12 19:12:56 -0500

    feat: preregistration form validation and data

---
+++ my-turborepo/apps/nextjs/src/app/preregistration/validation.ts
@@ -0,0 +1,9 @@
+import zfd from "zod-form-data";
+import z from "zod"
+
+export const preregistrationSchema = zfd.formData({
+    email: zfd.text(z.string().email().max(256).min(1)),
+    confirmation: zfd.checkbox(),
+})
+
+export type PreregistrationData = z.infer<typeof preregistrationSchema>;
