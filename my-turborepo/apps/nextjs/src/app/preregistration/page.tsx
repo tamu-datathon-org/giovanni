a67b37bcf29084288eb7840180bb582e39501051
redninjale <redninjale@gmail.com>
2024-08-12 19:12:56 -0500

    feat: preregistration form validation and data

---
+++ my-turborepo/apps/nextjs/src/app/preregistration/page.tsx
@@ -0,0 +1,8 @@
+import { PreregistrationForm } from "../_components/preregistration-form";
+
+// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
+export default function Page() {
+    return (
+        < PreregistrationForm />
+    )
+}
