1728f199f1d9b790d3e80b3e04268cd74bb98819
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-08-27 17:08:03 -0500

    Giving up on comprehensive account linking

--- my-turborepo/apps/nextjs/src/app/dashboard/account/link/page.tsx
+++ my-turborepo/apps/nextjs/src/app/dashboard/account/link/page.tsx
@@ -1,37 +1,31 @@
-import { signIn } from "@vanni/auth";
-
-export const runtime = "edge";
-function LinkButton({
-  connectionId,
-  redirectUri,
-  buttonText,
-}: {
-  connectionId: string;
-  redirectUri?: string;
-  buttonText: string;
-}) {
-  return (
-    <Button
-      size="lg"
-      formAction={async () => {
-        "use server";
-        await signIn("auth0", redirectUri ? { redirectTo: redirectUri } : {}, {
-          connection: connectionId,
-        });
-      }}
-    >
-      {buttonText}
-    </Button>
-  );
-}
-export default function LinkPage() {
-  // You can await this here if you don't want to show Suspense fallback below
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-        Link account
-      </h1>
-    </div>
-  );
-}
+// import { redirect } from "next/navigation";
+//
+// import { signIn } from "@vanni/auth";
+//
+// export const runtime = "edge";
+//
+// export default async function LinkPage() {
+//   // You can await this here if you don't want to show Suspense fallback below
+//   return (
+//     <div className="flex flex-col items-center justify-center gap-4">
+//       <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+//         Link account
+//       </h1>
+//       <h2>Linked accounts:</h2>
+//       <p>(insert accounts here)</p>
+//       <h2>Link new account:</h2>
+//       <p>
+//         You can link new accounts with the button below. Please note that this
+//         will sign you out.
+//       </p>
+//       <form>
+//         <button
+//           type="submit"
+//           onClick={redirect("/dashboard/account/link/stage2")}
+//         >
+//           Link new account
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
