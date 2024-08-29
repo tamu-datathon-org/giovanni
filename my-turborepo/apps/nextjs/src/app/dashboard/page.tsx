002dd45656700d3b785d21286d7db08908354582
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-08-29 15:20:53 -0500

    Refactored Dashboard Page

--- my-turborepo/apps/nextjs/src/app/dashboard/page.tsx
+++ my-turborepo/apps/nextjs/src/app/dashboard/page.tsx
@@ -7,19 +7,35 @@ const pages = [
   { href: "/discord", name: "Link with Discord" },
 ];
 
+function Button({ href, name }: { href: string; name: string }) {
+  return (
+    <a href={href} className="rounded-md bg-blue-500 px-4 py-2 text-white">
+      {name}
+    </a>
+  );
+}
+
+function ButtonContainer() {
+  return (
+    <div className="flex flex-col gap-2">
+      <ul>
+        {pages.map((page) => (
+          <li>
+            <Button href={page.href} name={page.name} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 export default function DashboardPage() {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
         Dashboard
       </h1>
-      <div className="flex flex-col gap-2">
-        <ul>
-          {pages.map((page) => (
-            <li key={page.href}>{page.name}</li>
-          ))}
-        </ul>
-      </div>
+      <ButtonContainer />
     </div>
   );
 }
