68513dc91f97dd688bc6d86ff2da496848c89a31
Betheony <bethany@code2college.org>
2024-09-03 21:09:07 -0500

    fix lint

--- my-turborepo/apps/nextjs/src/app/login/page.tsx
+++ my-turborepo/apps/nextjs/src/app/login/page.tsx
@@ -1,8 +1,10 @@
-import { signIn } from "@vanni/auth";
-import { Button } from "@vanni/ui/button";
 import Image from "next/image";
 import { AiOutlineClose } from "react-icons/ai";
-import '../_components/customCss.scss';
+
+import { signIn } from "@vanni/auth";
+import { Button } from "@vanni/ui/button";
+
+import "../_components/customCss.scss";
 
 export const runtime = "edge";
 
@@ -16,7 +18,8 @@ function LoginButton({
   buttonText: string;
 }) {
   return (
-    <Button className='bg-[#f5f5f5] compStyling border border-black text-black hover:bg-[#e4e3e4] hover:text-black w-full'
+    <Button
+      className="compStyling w-full border border-black bg-[#f5f5f5] text-black hover:bg-[#e4e3e4] hover:text-black"
       size="lg"
       formAction={async () => {
         "use server";
@@ -32,54 +35,65 @@ function LoginButton({
 
 export default function LoginPage() {
   return (
-    <main className="font-XPfont bg-large-device bg-cover bg-no-repeat bg-center h-screen w-screen flex items-center justify-center">
-        <div className="flex flex-col justify-center items-center xpBorder p-2 w-2/5">
-            <div className="flex flex-row w-full items-center justify-center">
-                <div className="w-full pr-2 lg:visible invisible"> {/**Random Lines */}
-                    <div className="horizontal-line"></div>
-                    <div className="horizontal-line"></div>
-                    <div className="horizontal-line"></div>
-                    <div className="horizontal-line"></div>
-                    <div className="horizontal-line"></div>
-                    <div className="horizontal-line"></div>
-                    <div className="horizontal-line"></div>
-                    <div className="horizontal-line"></div>
-                </div>
-                <Button className="compStyling lg:visible invisible"><AiOutlineClose className="close"/></Button>
-            </div>
-            <div
-                className='flex flex-col justify-center items-center relative shadow-inner w-full bg-[#e4e3e4] border border-black p-8 mt-2'>
-                <h1 className="text-6xl text-center">
-                    <span className="odd:text-teal-400">T</span>
-                    <span className="even:text-cyan-700">A</span>
-                    <span className="odd:text-teal-400 ">M</span>
-                    <span className="even:text-cyan-700">U</span> DATATHON </h1>
-                <h1 className="text-5xl tracking-tight">
-                    Log In
-                </h1>
-                <div className='w-full text-[#9c9c9c]'>
-                    <text className=''>Post version 202.4</text>
-                    <hr className="absolute left-0 w-2/5 h-0.5 bg-[#9c9c9c] border-0 rounded dark:bg-[#e4e3e4]"></hr>
-
-                </div>
-                {/*<AuthShowcase />*/}
-
-                <div className='w-1/2 pt-4'>
-                    <form className="flex flex-col w-full gap-2">
-                        <LoginButton connectionId="apple" buttonText="Sign in with Apple"/>
-                        <LoginButton connectionId="github" buttonText="Sign in with GitHub"/>
-                        <LoginButton
-                            connectionId="google-oauth2"
-                            buttonText="Sign in with Google"
-                        />
-                    </form>
-                </div>
-
-                <Image src="/Pixel_PolarBear.png" className="absolute -bottom-4 -right-5 invisible lg:visible "
-                       width={200}
-                       height={200} alt="polar bear"/>
-            </div>
+    <main className="font-XPfont bg-large-device flex h-screen w-screen items-center justify-center bg-cover bg-center bg-no-repeat">
+      <div className="xpBorder flex w-2/5 flex-col items-center justify-center p-2">
+        <div className="flex w-full flex-row items-center justify-center">
+          <div className="invisible w-full pr-2 lg:visible">
+            {" "}
+            {/**Random Lines */}
+            <div className="horizontal-line"></div>
+            <div className="horizontal-line"></div>
+            <div className="horizontal-line"></div>
+            <div className="horizontal-line"></div>
+            <div className="horizontal-line"></div>
+            <div className="horizontal-line"></div>
+            <div className="horizontal-line"></div>
+            <div className="horizontal-line"></div>
+          </div>
+          <Button className="compStyling invisible lg:visible">
+            <AiOutlineClose className="close" />
+          </Button>
         </div>
+        <div className="relative mt-2 flex w-full flex-col items-center justify-center border border-black bg-[#e4e3e4] p-8 shadow-inner">
+          <h1 className="text-center text-6xl">
+            <span className="odd:text-teal-400">T</span>
+            <span className="even:text-cyan-700">A</span>
+            <span className="odd:text-teal-400 ">M</span>
+            <span className="even:text-cyan-700">U</span> DATATHON{" "}
+          </h1>
+          <h1 className="text-5xl tracking-tight">Log In</h1>
+          <div className="w-full text-[#9c9c9c]">
+            <text className="">Post version 202.4</text>
+            <hr className="absolute left-0 h-0.5 w-2/5 rounded border-0 bg-[#9c9c9c] dark:bg-[#e4e3e4]"></hr>
+          </div>
+          {/*<AuthShowcase />*/}
+
+          <div className="w-1/2 pt-4">
+            <form className="flex w-full flex-col gap-2">
+              <LoginButton
+                connectionId="apple"
+                buttonText="Sign in with Apple"
+              />
+              <LoginButton
+                connectionId="github"
+                buttonText="Sign in with GitHub"
+              />
+              <LoginButton
+                connectionId="google-oauth2"
+                buttonText="Sign in with Google"
+              />
+            </form>
+          </div>
+
+          <Image
+            src="/Pixel_PolarBear.png"
+            className="invisible absolute -bottom-4 -right-5 lg:visible "
+            width={200}
+            height={200}
+            alt="polar bear"
+          />
+        </div>
+      </div>
     </main>
   );
 }
