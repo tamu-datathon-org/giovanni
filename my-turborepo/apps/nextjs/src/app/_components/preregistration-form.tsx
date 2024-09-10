dff4650efa34fdf29844e3be998f742d0834d05e
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-09-10 17:02:34 -0500

    Refactored preregistration-form slightly

--- my-turborepo/apps/nextjs/src/app/_components/preregistration-form.tsx
+++ my-turborepo/apps/nextjs/src/app/_components/preregistration-form.tsx
@@ -17,6 +17,63 @@ import { TRPCError } from "@trpc/server";
 import { TRPCClientError } from "@trpc/client";
 
 
+function Lines() {
+    return <div className="w-full pr-3"> {/**Random Lines */}
+        <div className="horizontal-line"></div>
+        <div className="horizontal-line"></div>
+        <div className="horizontal-line"></div>
+        <div className="horizontal-line"></div>
+        <div className="horizontal-line"></div>
+        <div className="horizontal-line"></div>
+        <div className="horizontal-line"></div>
+        <div className="horizontal-line"></div>
+    </div>;
+}
+
+function ExitButton() {
+    // This button is only there for visual purposes
+    return <Button className="compStyling"><AiOutlineClose className="close"/></Button>;
+}
+
+function TitleText() {
+    return <h1 className="md:text-6xl text-5xl p-10 pb-5">
+        <span className="odd:text-teal-400">T</span>
+        <span className="even:text-cyan-700">A</span>
+        <span className="odd:text-teal-400 ">M</span>
+        <span className="even:text-cyan-700">U</span> Datathon Preregistration</h1>;
+}
+
+function EmailBox(props: { register: any, errors: any }) {
+    return <>
+        <label className="flex flex-row justify-center ">
+            <h1 className="pr-4">Enter Email: </h1>
+            <div className="flex rounded-sm bg-black p-0.5">
+                <input {...props.register}
+                       className=" border-cyan-600"/>
+            </div>
+        </label>
+        {props.errors.email?.message != undefined && (
+            <div className="text-sm text-red-600">
+                Invalid Email
+            </div>)}
+    </>;
+}
+
+function TermsAndConditions(props: { register: any, errors: any }) {
+    return <>
+        <label className="text-blac">
+            <input className="m-1" type="checkbox"
+                   value={"on"} {...props.register} />
+            <span>I agree to the terms and conditions.</span>
+        </label>
+        {props.errors.confirmation?.message != undefined && (
+            <div className="text-sm text-red-600">
+                Missing Field
+            </div>
+        )}
+    </>;
+}
+
 export const CreatePreregistrationForm = () => {
     const { toast } = useToast()
 
@@ -46,45 +103,17 @@ export const CreatePreregistrationForm = () => {
     return (
         <div className="font-XPfont font-bold">
             <div className="flex h-screen items-center justify-center">
-                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center text-center lg:w-2/5 w-11/12 text-lg xpBorder ">
+                <form onSubmit={handleSubmit(onSubmit)}
+                      className="flex flex-col items-center text-center lg:w-2/5 w-11/12 text-lg xpBorder ">
                     <div className="flex flex-row w-full items-center justify-center">
-                        <div className="w-full pr-3"> {/**Random Lines */}
-                            <div className="horizontal-line"></div>
-                            <div className="horizontal-line"></div>
-                            <div className="horizontal-line"></div>
-                            <div className="horizontal-line"></div>
-                            <div className="horizontal-line"></div>
-                            <div className="horizontal-line"></div>
-                            <div className="horizontal-line"></div>
-                            <div className="horizontal-line"></div>
-                        </div>
-                        <Button className="compStyling"><AiOutlineClose className="close" /></Button>
+                        <Lines/>
+                        <ExitButton/>
                     </div>
-                    <div className="flex flex-col relative items-center overflow-hidden bg-[#e4e3e4] w-full mt-3 border-[#585958] lg:border-[1px] border-0">
-                        <h1 className="md:text-6xl text-5xl p-10 pb-5">
-                            <span className="odd:text-teal-400">T</span>
-                            <span className="even:text-cyan-700">A</span>
-                            <span className="odd:text-teal-400 ">M</span>
-                            <span className="even:text-cyan-700">U</span> Datathon Preregistration</h1>
-                        <label className="flex flex-row justify-center ">
-                            <h1 className="pr-4">Enter Email:  </h1>
-                            <div className="flex rounded-sm bg-black p-0.5">
-                                <input {...register("email", { required: true, maxLength: 256 })} className=" border-cyan-600" />
-                            </div>
-                        </label>
-                        {errors.email?.message != undefined && (
-                            <div className="text-sm text-red-600">
-                                Invalid Email
-                            </div>)}
-                        <label className="text-blac">
-                            <input className="m-1" type="checkbox" value={"on"} {...register("confirmation", { required: true, })} />
-                            <span>I agree to the terms and conditions.</span>
-                        </label>
-                        {errors.confirmation?.message != undefined && (
-                            <div className="text-sm text-red-600">
-                                Missing Field
-                            </div>
-                        )}
+                    <div
+                        className="flex flex-col relative items-center overflow-hidden bg-[#e4e3e4] w-full mt-3 border-[#585958] lg:border-[1px] border-0">
+                        <TitleText/>
+                        <EmailBox register={register("email", {required: true, maxLength: 256})} errors={errors}/>
+                        <TermsAndConditions register={register("confirmation", {required: true,})} errors={errors}/>
                         <Button
                             className="xpBorder text-xl font-extrabold my-4 submitBtn w-fit bg-cyan-700"
                             type="submit"
@@ -92,15 +121,18 @@ export const CreatePreregistrationForm = () => {
                         >
                             {
                                 isSubmitting ?
-                                    <Image src="loading.svg" className="animate-spin" width={24} height={24} aria-hidden="true" alt="loading..." />
+                                    <Image src="loading.svg" className="animate-spin" width={24} height={24}
+                                           aria-hidden="true" alt="loading..."/>
                                     : "Submit"
                             }
                         </Button>
-                        <Image src="/Pixel_PolarBear.png" className="absolute -bottom-5 -right-5 xl:size-44 lg:size-28 md:size-56 size-32" width={100} height={100} alt="polar bear" />
+                        <Image src="/Pixel_PolarBear.png"
+                               className="absolute -bottom-5 -right-5 xl:size-44 lg:size-28 md:size-56 size-32"
+                               width={100} height={100} alt="polar bear"/>
                     </div>
                 </form>
-            </div >
-        </div >
+            </div>
+        </div>
     )
 }
 
