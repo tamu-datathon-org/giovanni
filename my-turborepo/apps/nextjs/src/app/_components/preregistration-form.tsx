cd7605ad910cba491cff8a5f746604c179bc53dc
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-09-12 15:07:42 -0500

    Rearranged form tag

--- my-turborepo/apps/nextjs/src/app/_components/preregistration-form.tsx
+++ my-turborepo/apps/nextjs/src/app/_components/preregistration-form.tsx
@@ -158,36 +158,35 @@ export const CreatePreregistrationForm = () => {
   return (
     <div className="font-XPfont font-bold">
       <div className="flex h-screen flex-col items-center justify-center ">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="xpBorder m-5 flex w-11/12 flex-col items-center text-center text-lg lg:w-2/5"
-        >
+        <div className="xpBorder m-5 flex w-11/12 flex-col items-center text-center text-lg lg:w-2/5">
           <div className="flex w-full flex-row items-center justify-center">
             <Lines />
             <ExitButton />
           </div>
           <div className="relative mt-3 flex w-full flex-col items-center overflow-hidden border-0 border-[#585958] bg-[#e4e3e4] lg:border-[1px]">
             <TitleText />
-            <EmailBox register={register} errors={errors} />
-            <TermsAndConditions register={register} errors={errors} />
-            <Button
-              className="xpBorder submitBtn my-4 w-fit bg-cyan-700 text-xl font-extrabold"
-              type="submit"
-              disabled={!isDirty || isSubmitting}
-            >
-              {isSubmitting ? (
-                <Image
-                  src="loading.svg"
-                  className="animate-spin"
-                  width={24}
-                  height={24}
-                  aria-hidden="true"
-                  alt="loading..."
-                />
-              ) : (
-                "Submit"
-              )}
-            </Button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <EmailBox register={register} errors={errors} />
+              <TermsAndConditions register={register} errors={errors} />
+              <Button
+                className="xpBorder submitBtn my-4 w-fit bg-cyan-700 text-xl font-extrabold"
+                type="submit"
+                disabled={!isDirty || isSubmitting}
+              >
+                {isSubmitting ? (
+                  <Image
+                    src="loading.svg"
+                    className="animate-spin"
+                    width={24}
+                    height={24}
+                    aria-hidden="true"
+                    alt="loading..."
+                  />
+                ) : (
+                  "Submit"
+                )}
+              </Button>
+            </form>
             <Image
               src="/Pixel_PolarBear.png"
               className="absolute -bottom-5 -right-5 size-32 md:size-56 lg:size-28 xl:size-44"
@@ -196,7 +195,7 @@ export const CreatePreregistrationForm = () => {
               alt="polar bear"
             />
           </div>
-        </form>
+        </div>
         {/* <IconList /> */}
       </div>
     </div>
