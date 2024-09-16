cc233e278dda70df54e659e84819cd6ede52ba82
redninjale <redninjale@gmail.com>
2024-09-15 19:47:02 -0500

    moving data from cambranch 1/2

---
+++ my-turborepo/apps/nextjs/src/app/apply/application/page.tsx
@@ -0,0 +1,12 @@
+import { ApplicationForm } from "~/app/_components/application-form";
+
+export default function Page() {
+  return (
+    <>
+      <div className="mainContent">
+        <h1>APPLICATION</h1>
+        < ApplicationForm />
+      </div>
+    </>
+  );
+}
