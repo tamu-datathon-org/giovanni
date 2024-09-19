f7492f0dbe3e3f1349689827e088897152135978
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-09-18 20:38:16 -0500

    Fixed unresolved images/fonts for real

--- my-turborepo/apps/nextjs/src/app/preregistration/layout.tsx
+++ my-turborepo/apps/nextjs/src/app/preregistration/layout.tsx
@@ -8,7 +8,7 @@ export default function PreregistrationLayout({
   return (
     <>
       <Image
-        src="/wallpaper.webp"
+        src="/public/wallpaper.webp"
         alt="Preregistration background"
         fill={true}
         objectFit="cover"
