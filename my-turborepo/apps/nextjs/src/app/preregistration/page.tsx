1f34496d6196751a35575463197a9a9ae124072b
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-09-18 20:19:32 -0500

    Moved image locations Added webp versions of background images

--- my-turborepo/apps/nextjs/src/app/preregistration/page.tsx
+++ my-turborepo/apps/nextjs/src/app/preregistration/page.tsx
@@ -1,14 +1,15 @@
 import { CreatePreregistrationForm } from "../_components/preregistration-form";
-import IconList from "../_components/IconList"; // Import the 'IconList' component and its props type
+
+// import IconList from "../_components/IconList"; // Import the 'IconList' component and its props type
 
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 export default function Page() {
-    return (
-        <div className="overflow-hidden h-screen w-screen lg:bg-large-device bg-mobile-device bg-cover bg-no-repeat bg-center ">
-            <div className="z-30 relative">
-                <CreatePreregistrationForm />
-            </div>
-            <div className="absolute top-0 left-0 w-screen bg-black h-screen opacity-40"></div>
-        </div>
-    )
+  return (
+    <div className="lg:bg-large-device bg-mobile-device h-screen w-screen overflow-hidden bg-cover bg-center bg-no-repeat ">
+      <div className="relative z-30">
+        <CreatePreregistrationForm />
+      </div>
+      <div className="absolute left-0 top-0 h-screen w-screen bg-black opacity-40"></div>
+    </div>
+  );
 }
