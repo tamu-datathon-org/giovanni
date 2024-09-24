abb6501ba5ffdff29ddadf26bfdd388c5c24d764
Samay Upadhyay <upadsamay387@gmail.com>
2024-09-24 18:23:47 -0500

    Recreated dashboard layout

--- my-turborepo/apps/nextjs/src/app/dashboard/layout.tsx
+++ my-turborepo/apps/nextjs/src/app/dashboard/layout.tsx
@@ -1,38 +1,18 @@
-import type { Metadata, Viewport } from "next";
-import { SessionProvider } from "next-auth/react";
+import BackgroundImage from "~/app/_components/images/background";
 
-import "~/app/globals.css";
-
-import { env } from "~/env";
-
-export const metadata: Metadata = {
-  metadataBase: new URL(
-    env.VERCEL_ENV === "production"
-      ? "https://turbo.t3.gg"
-      : "http://localhost:3000",
-  ),
-  title: "Create T3 Turbo",
-  description: "Simple monorepo with shared backend for web & mobile apps",
-  openGraph: {
-    title: "Create T3 Turbo",
-    description: "Simple monorepo with shared backend for web & mobile apps",
-    url: "https://create-t3-turbo.vercel.app",
-    siteName: "Create T3 Turbo",
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@jullerino",
-    creator: "@jullerino",
-  },
-};
-
-export const viewport: Viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-};
-
-export default function AccountLayout(props: { children: React.ReactNode }) {
-  return <SessionProvider>{props.children}</SessionProvider>;
+export default function PreregistrationLayout({
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <BackgroundImage
+        desktop_src={"/assets/wallpaper.png"}
+        mobile_src={"/assets/wallpaper-mobile.png"}
+        alt={"Dashboard background"}
+      />
+      <div className="max-h-screen-sm overflow-hidden">{children}</div>
+    </>
+  );
 }
