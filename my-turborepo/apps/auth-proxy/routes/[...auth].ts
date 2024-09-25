bd4fe63a2a166ea782c5253486fcb2703fcf9af7
Samay Upadhyay <upadsamay387@gmail.com>
2024-09-25 00:40:52 -0500

    Added auth0 provider to auth-proxy

--- my-turborepo/apps/auth-proxy/routes/[...auth].ts
+++ my-turborepo/apps/auth-proxy/routes/[...auth].ts
@@ -1,6 +1,7 @@
 import { Auth } from "@auth/core";
 import Discord from "@auth/core/providers/discord";
 import { eventHandler, toWebRequest } from "h3";
+import Auth0 from "next-auth/providers/auth0";
 
 export default eventHandler(async (event) =>
   Auth(toWebRequest(event), {
@@ -11,6 +12,11 @@ export default eventHandler(async (event) =>
       Discord({
         clientId: process.env.AUTH_DISCORD_ID,
         clientSecret: process.env.AUTH_DISCORD_SECRET,
+      }),
+      Auth0({
+        clientId: env.AUTH_AUTH0_ID,
+        clientSecret: env.AUTH_AUTH0_SECRET,
+        issuer: env.AUTH_AUTH0_DOMAIN,
       }),
     ],
   }),
