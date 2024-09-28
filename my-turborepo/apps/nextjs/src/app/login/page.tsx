1dcf7928cb70ca565de23dc5de84f60be3fafc43
Betheony <bt.bethanyt@gmail.com>
2024-09-28 14:49:28 -0500

    final - without retro cursor or draggable

--- my-turborepo/apps/nextjs/src/app/login/page.tsx
+++ my-turborepo/apps/nextjs/src/app/login/page.tsx
@@ -21,7 +21,7 @@ function LoginButton({
 }) {
   return (
     <Button
-      className="compStyling w-full border border-black bg-[#f5f5f5] text-black hover:bg-[#e4e3e4] hover:text-black"
+      className="compStyling2 border border-black bg-[#f5f5f5] text-black hover:bg-[#e4e3e4] hover:text-black"
       size="lg"
       formAction={async () => {
         "use server";
@@ -37,11 +37,10 @@ function LoginButton({
 
 export default function LoginPage() {
   return (
-    <main className="font-XPfont bg-large-device flex h-screen w-screen items-center justify-center bg-cover bg-center bg-no-repeat">
-        <DraggableComponent />
-      <div className="flex flex-col xpBorder items-center justify-center p-2">
+    <main className="flex font-XPfont bg-large-device h-screen w-screen items-center justify-center bg-cover bg-center bg-no-repeat">
+      <div className="flex max-w-full flex-col xpBorder items-center justify-center">
         <div className="flex w-full flex-row items-center justify-center">
-          <div className="invisible w-full pr-2 lg:visible">
+          <div className="w-full pr-2">
             {" "}
             {/**Random Lines */}
             <div className="horizontal-line"></div>
@@ -53,11 +52,11 @@ export default function LoginPage() {
             <div className="horizontal-line"></div>
             <div className="horizontal-line"></div>
           </div>
-          <Button className="compStyling invisible lg:visible">
+          <Button className="compStyling2">
             <AiOutlineClose className="close" />
           </Button>
         </div>
-        <div className="relative mt-2 flex w-full flex-col items-center justify-center border border-black bg-[#e4e3e4] p-8 shadow-inner">
+        <div className="relative mt-2 flex w-full flex-col items-center justify-center border border-black bg-[#e4e3e4] p-8 px-16">
           <h1 className="text-center text-6xl">
             <span className="odd:text-teal-400">T</span>
             <span className="even:text-cyan-700">A</span>
@@ -65,13 +64,13 @@ export default function LoginPage() {
             <span className="even:text-cyan-700">U</span> DATATHON{" "}
           </h1>
           <h1 className="text-5xl tracking-tight">Log In</h1>
-          {/*<div className="w-full text-[#9c9c9c]">*/}
-          {/*  <text className="">Post version 202.4</text>*/}
-          {/*  <hr className="absolute left-0 h-0.5 w-2/5 rounded border-0 bg-[#9c9c9c] dark:bg-[#e4e3e4]"></hr>*/}
-          {/*</div>*/}
+          <div className="w-full text-[#9c9c9c]">
+            <text className="">Post version 202.4</text>
+            <hr className="absolute left-0 h-0.5 w-2/5 rounded border-0 bg-[#9c9c9c] dark:bg-[#e4e3e4]"></hr>
+          </div>
           {/*<AuthShowcase />*/}
 
-          <div className="w-1/2 pt-4">
+          <div className="w-3/5 pt-4">
             <form className="flex w-full flex-col gap-2">
               <LoginButton
                 connectionId="apple"
