b5d1dc02b57a0b24dc64d6fc80a15ccc3984fe29
Samay Upadhyay <upadsamay387@gmail.com>
2024-09-28 21:15:14 -0500

    Revert "Edited something in next.config.js"
    This reverts commit 8871407db551f70de4883aecdc170d2cc9646a58.

--- my-turborepo/apps/nextjs/next.config.js
+++ my-turborepo/apps/nextjs/next.config.js
@@ -1,6 +1,5 @@
 import { fileURLToPath } from "url";
 import createJiti from "jiti";
-import path from "path";
 
 // Import env files to validate at build time. Use jiti so we can load .ts files in here.
 createJiti(fileURLToPath(import.meta.url))("./src/env");
@@ -17,7 +16,7 @@ const config = {
     "@vanni/ui",
     "@vanni/validators",
   ],
-  outputFileTracingRoot: path.join(__dirname, '../../'),
+
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
