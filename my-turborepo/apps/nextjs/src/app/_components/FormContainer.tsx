168708d79b15ab3f4e52d4cc0d4605a0e80a0588
aniketshirodkar <shirodkar.aniket@gmail.com>
2024-09-30 08:52:45 -0500

    fix component gray bg

--- my-turborepo/apps/nextjs/src/app/_components/FormContainer.tsx
+++ my-turborepo/apps/nextjs/src/app/_components/FormContainer.tsx
@@ -23,11 +23,11 @@ const FormContainer: React.FC<FormContainerProps> = ({
           className="xpBorder m-5 flex w-11/12 flex-col items-center text-center text-lg lg:w-2/5"
         >
           {/* <div className = "border-[#585958]"> */}
-          
+          <div className="relative mt-3 flex w-full flex-col items-center overflow-hidden border-0 border-[#585958] bg-[#e4e3e4] lg:border-[1px]">
           <div className="flex w-full flex-col items-center justify-center">
             {children}
           </div>
-          <div className="relative mt-3 flex w-full h-full flex-col items-center overflow-hidden border-0 border-[#585958] bg-[#e4e3e4] lg:border-[1px]">
+          {/* <div className="relative mt-3 flex w-full h-full flex-col items-center overflow-hidden border-0 border-[#585958] bg-[#e4e3e4] lg:border-[1px]"> */}
             <Button
               className="xpBorder submitBtn my-4 w-fit bg-cyan-700 text-xl font-extrabold"
               type="submit"
@@ -53,8 +53,9 @@ const FormContainer: React.FC<FormContainerProps> = ({
               height={100}
               alt="polar bear"
             />
-          </div>
           {/* </div> */}
+          {/* </div> */}
+          </div>
         </form>
       </div>
     </div>
