7e7947b016f7e92d663b7a24c7c19d12512d88fb
Samay Upadhyay <upadsamay387@gmail.com>
2024-10-01 21:08:06 -0500

    Added more options for years, and set "other" to be a numeric value

--- my-turborepo/apps/nextjs/src/lib/dropdownOptions.ts
+++ my-turborepo/apps/nextjs/src/lib/dropdownOptions.ts
@@ -399,12 +399,16 @@ export const EDUCATION_LEVELS = [
 ];
 
 export const GRADUATION_YEARS = [
+  { value: "2022", label: "2022" },
   { value: "2023", label: "2023" },
   { value: "2024", label: "2024" },
   { value: "2025", label: "2025" },
   { value: "2026", label: "2026" },
   { value: "2027", label: "2027" },
-  { value: "Other", label: "Other" },
+  { value: "2028", label: "2028" },
+  { value: "2029", label: "2029" },
+  { value: "2030", label: "2030" },
+  { value: "9999", label: "Other" },
 ];
 
 export const GENDER_OPTIONS = [
