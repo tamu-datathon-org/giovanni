db83ff2926c8bf83386b9af3940deed962f8cb47
redninjale <redninjale@gmail.com>
2024-10-07 11:43:34 -0500

    minor queries

--- my-turborepo/packages/api/src/router/email.ts
+++ my-turborepo/packages/api/src/router/email.ts
@@ -1,9 +1,26 @@
+import { asc } from "@vanni/db";
 import { protectedProcedure } from "../trpc";
+import { EmailLabel } from "@vanni/db/schema";
 
 export const emailRouter = {
     getAllLabels: protectedProcedure
         .query(async ({ ctx }) => {
-            const emailLabels = await ctx.db.query.EmailLabel.findMany();
+            const emailLabels = await ctx.db.query.EmailLabel.findMany({
+                orderBy: asc(EmailLabel.name),
+                columns: {
+                    name: true,
+                }
+            });
+            console.log(emailLabels);
             return emailLabels;
-        })
+        }),
+    getRegistration: protectedProcedure
+        .query(async ({ ctx }) => {
+            return await ctx.db.query.Preregistration.findMany();
+        }),
+    getAllEmails: protectedProcedure
+        .query(async ({ ctx }) => {
+            const emails = await ctx.db.query.EmailList.findMany();
+            return emails;
+        }),
 }
