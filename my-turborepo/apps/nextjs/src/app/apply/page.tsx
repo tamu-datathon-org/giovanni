bdc2782ed1cec9a406e076b3adc3e252f93913e2
Samay Upadhyay <upadsamay387@gmail.com>
2024-11-04 20:58:08 +0000

    Changed application-related redirects

--- my-turborepo/apps/nextjs/src/app/apply/page.tsx
+++ my-turborepo/apps/nextjs/src/app/apply/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
-import { redirect } from "next/navigation";
-
-import IconList from "../_components/IconList";
 
 export default function Page() {
-  redirect("/apply/application");
+  // TODO: Replace this with an API call to the correct router
+  const appStatus = "pending";
+
+  // TODO: Replace this variable with an api route that checks the latest event
+  const appsOpen = false;
   return (
     <>
       {/* <IconList /> */}
@@ -21,27 +22,35 @@ export default function Page() {
             padding: "10px",
           }}
         >
-          {/* <div className="dashboardText">APPLICATION STATUS:</div>
+          <div className="dashboardText">APPLICATION STATUS:</div>
           <div className="dashStatus">{appStatus.toUpperCase()}</div>
 
-          <div className="dashboardText">
-            <br />
-            Applications are currently closed.
-            <br />
-            We are currently reviewing applications.
-            <br />
-            Keep an eye out for an email!
-          </div>
-          <button className="editButton">
-            <Link
-              className="dashboardText buttonText"
-              href="/apply/application"
-            >
-              Edit your application
-            </Link>
-          </button> */}
+          {appsOpen ? <AppsOpenMessage /> : <AppsClosedMessage />}
         </form>
       </div>
     </>
+  );
+}
+
+function AppsClosedMessage() {
+  return (
+    <div className="dashboardText">
+      <br />
+      Applications are currently closed.
+      <br />
+      We are currently reviewing applications.
+      <br />
+      Keep an eye out for an email!
+    </div>
+  );
+}
+
+function AppsOpenMessage() {
+  return (
+    <button className="editButton">
+      <Link className="dashboardText buttonText" href="/apply/application">
+        Edit your application
+      </Link>
+    </button>
   );
 }
