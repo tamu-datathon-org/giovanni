10088aad5db09c9e483c2c4ce56c206da67acc20
Samay Upadhyay <upadsamay387@gmail.com>
2024-11-09 09:45:06 -0600

    Removed MLH Workshop

--- my-turborepo/apps/nextjs/src/app/schedule/page.tsx
+++ my-turborepo/apps/nextjs/src/app/schedule/page.tsx
@@ -69,7 +69,8 @@ const events: Event[] = [
     name: "TD Workshop: Intro to Git",
     startDate: new Date("2024-11-09T13:30:00"),
     endDate: new Date("2024-11-09T14:00:00"),
-    description: "You've probably heard of git or GitHub before but what is it? Why do we use it? How can you set it up and use it for yourself? This workshop answers all this and more!",
+    description:
+      "You've probably heard of git or GitHub before but what is it? Why do we use it? How can you set it up and use it for yourself? This workshop answers all this and more!",
     location: "MSC 2406B",
   },
   {
@@ -85,7 +86,8 @@ const events: Event[] = [
     name: "TAMIDS Workshop: Fundamentals of Image Classification with a Simple Neural Network",
     startDate: new Date("2024-11-09T14:30:00"),
     endDate: new Date("2024-11-09T15:00:00"),
-    description: "Participants will focus on fundamental aspects of image classification using the Keras API with a handwritten digit dataset. They will learn key steps including image (matrix) structures, dataset loading and preparation, neural network construction for classification, and model training. Through hands-on exercises, participants will understand the fundamental components of machine learning, especially neural networks.",
+    description:
+      "Participants will focus on fundamental aspects of image classification using the Keras API with a handwritten digit dataset. They will learn key steps including image (matrix) structures, dataset loading and preparation, neural network construction for classification, and model training. Through hands-on exercises, participants will understand the fundamental components of machine learning, especially neural networks.",
     location: "MSC 2406B",
   },
   {
@@ -97,19 +99,12 @@ const events: Event[] = [
     location: "MSC 2406B",
   },
   {
-    id: 10,
-    name: "Workshop: MLH",
-    startDate: new Date("2024-11-09T15:30:00"),
-    endDate: new Date("2024-11-09T16:00:00"),
-    description: "Workshop hosted by MLH.",
-    location: "MSC 2406B",
-  },
-  {
     id: 11,
     name: "Baker Hughes: Data Downsampling Techniques for Machine Learning Efficiency",
     startDate: new Date("2024-11-09T16:00:00"),
     endDate: new Date("2024-11-09T16:30:00"),
-    description: "Workshop hosted by Baker Hughes on data downsampling techniques for ML efficiency.",
+    description:
+      "Workshop hosted by Baker Hughes on data downsampling techniques for ML efficiency.",
     location: "MSC 2406B",
   },
   {
@@ -117,7 +112,8 @@ const events: Event[] = [
     name: "TD Workshop: Quick and Dirty ML with SKLearn",
     startDate: new Date("2024-11-09T16:30:00"),
     endDate: new Date("2024-11-09T17:00:00"),
-    description: "Machine learning is a series of linear algebra, calculus, probability, statistics, and a lot of other advanced math concepts that you need a PHd to fully understand. But luckily, we don't need to know any of that to use it in our code! This workshop is a simple introduction to how you can make use of the power of numerous Machine Learning models using sklearn in just 5 lines of code!",
+    description:
+      "Machine learning is a series of linear algebra, calculus, probability, statistics, and a lot of other advanced math concepts that you need a PHd to fully understand. But luckily, we don't need to know any of that to use it in our code! This workshop is a simple introduction to how you can make use of the power of numerous Machine Learning models using sklearn in just 5 lines of code!",
     location: "MSC 2406B",
   },
   {
@@ -125,7 +121,8 @@ const events: Event[] = [
     name: "TD Workshop: Introduction to Game Solving Algorithms",
     startDate: new Date("2024-11-09T17:00:00"),
     endDate: new Date("2024-11-09T17:30:00"),
-    description: "We'll teach you the basics of the algorithms that are used to power the AI bots that play board games and give you hints on how to solve our challenge.",
+    description:
+      "We'll teach you the basics of the algorithms that are used to power the AI bots that play board games and give you hints on how to solve our challenge.",
     location: "MSC 2406B",
   },
   {
@@ -133,7 +130,8 @@ const events: Event[] = [
     name: "Workshop: Advanced GPT Usage",
     startDate: new Date("2024-11-09T17:30:00"),
     endDate: new Date("2024-11-09T18:00:00"),
-    description: "We'll cover interesting ways for you to apply GPT (and other LLMs) to provide more meaningful output, as well as how to use them beyond just their web interfaces.",
+    description:
+      "We'll cover interesting ways for you to apply GPT (and other LLMs) to provide more meaningful output, as well as how to use them beyond just their web interfaces.",
     location: "MSC 2406B",
   },
   {
@@ -141,7 +139,8 @@ const events: Event[] = [
     name: "Bob Ross Digital Painting Session",
     startDate: new Date("2024-11-09T18:00:00"),
     endDate: new Date("2024-11-09T18:45:00"),
-    description: "Follow along to a BOB ROSS tutorial with MS Paint (or equivalent app) using ONLY TOUCHPADS (NO PENS, TOUCHSCREENS, OR MOUSES ALLOWED). Post it in the Bob Ross channel for a chance to win some prizes!!",
+    description:
+      "Follow along to a BOB ROSS tutorial with MS Paint (or equivalent app) using ONLY TOUCHPADS (NO PENS, TOUCHSCREENS, OR MOUSES ALLOWED). Post it in the Bob Ross channel for a chance to win some prizes!!",
     location: "MSC 2406A",
   },
   {
@@ -157,7 +156,8 @@ const events: Event[] = [
     name: "Y2K Fashion Show",
     startDate: new Date("2024-11-09T20:00:00"),
     endDate: new Date("2024-11-09T21:00:00"),
-    description: "Showcase your y2k fashion! Winners get a prize! Come in your most amazing y2k fit with your best catwalk.",
+    description:
+      "Showcase your y2k fashion! Winners get a prize! Come in your most amazing y2k fit with your best catwalk.",
     location: "MSC 2406A",
   },
   {
@@ -208,7 +208,8 @@ const events: Event[] = [
     name: "Closing Ceremony",
     startDate: new Date("2024-11-10T15:00:00"),
     endDate: new Date("2024-11-10T16:00:00"),
-    description: "Join us for the closing ceremony where we announce the winners of TAMU Datathon!",
+    description:
+      "Join us for the closing ceremony where we announce the winners of TAMU Datathon!",
     location: "MSC Bethancourt Ballroom",
   },
   {
@@ -322,25 +323,29 @@ const SchedulePage: React.FC = () => {
         >
           <WindowContainer
             isOpen={true}
-            openFunc={() => { }}
+            openFunc={() => {}}
             borderGradientStart="#34a4eb"
             borderGradientMiddle="#004c99"
             borderGradientEnd="#124c87"
           >
-            <div className="flex tabletRange:w-[250px] h-[175px] w-[190px] flex-col items-center justify-between p-4 sm:h-[150px] sm:w-[350px] md:h-[150px] md:w-[500px] lg:h-[200px] lg:w-[400px] xl:h-[200px] xl:w-[600px]">
-              <h2 className="mb-2 text-center text-md font-bold text-black sm:text-lg md:text-2xl lg:text-3xl tabletRange:text-sm">
-                {nextEvent ? `Countdown to ${nextEvent.name}` : "No upcoming events"}
+            <div className="tabletRange:w-[250px] flex h-[175px] w-[190px] flex-col items-center justify-between p-4 sm:h-[150px] sm:w-[350px] md:h-[150px] md:w-[500px] lg:h-[200px] lg:w-[400px] xl:h-[200px] xl:w-[600px]">
+              <h2 className="text-md tabletRange:text-sm mb-2 text-center font-bold text-black sm:text-lg md:text-2xl lg:text-3xl">
+                {nextEvent
+                  ? `Countdown to ${nextEvent.name}`
+                  : "No upcoming events"}
               </h2>
-              <h3 className="text-xs font-bold text-black sm:text-lg md:text-xl lg:text-2xl tabletRange:text-sm">
-                {nextEvent ? format(nextEvent.startDate, "MMMM d, yyyy, h:mm a") : ""}
+              <h3 className="tabletRange:text-sm text-xs font-bold text-black sm:text-lg md:text-xl lg:text-2xl">
+                {nextEvent
+                  ? format(nextEvent.startDate, "MMMM d, yyyy, h:mm a")
+                  : ""}
               </h3>
-              <div className="flex justify-center space-x-2 sm:space-x-3 md:space-x-4 tabletRange:text-xs">
+              <div className="tabletRange:text-xs flex justify-center space-x-2 sm:space-x-3 md:space-x-4">
                 {Object.entries(timeLeft).map(([unit, value]) => (
                   <div key={unit} className="text-center">
-                    <div className="text-base font-bold text-black sm:text-lg md:text-2xl lg:text-3xl tabletRange:text-sm">
+                    <div className="tabletRange:text-sm text-base font-bold text-black sm:text-lg md:text-2xl lg:text-3xl">
                       {value}
                     </div>
-                    <div className="text-xs uppercase text-black sm:text-sm md:text-base lg:text-lg tabletRange:text-sm">
+                    <div className="tabletRange:text-sm text-xs uppercase text-black sm:text-sm md:text-base lg:text-lg">
                       {unit}
                     </div>
                   </div>
@@ -355,26 +360,29 @@ const SchedulePage: React.FC = () => {
           onFocus={setFocusedWindow}
           name="currentEvent"
           focus={focusedWindow}
-          className="absolute right-0 top-20 sm:left-8 sm:top-72 md:left-[90px] md:top-[375px] ipadRange:top-[320px] tabletRange:left-5"
+          className="ipadRange:top-[320px] tabletRange:left-5 absolute right-0 top-20 sm:left-8 sm:top-72 md:left-[90px] md:top-[375px]"
         >
           <WindowContainer
             isOpen={true}
-            openFunc={() => { }}
+            openFunc={() => {}}
             borderGradientStart="#34a4eb"
             borderGradientMiddle="#004c99"
             borderGradientEnd="#124c87"
           >
-            <div className="flex h-[220px] w-[175px] flex-col items-center justify-between p-4 sm:h-[150px] sm:w-[300px] md:h-[150px] md:w-[500px] lg:h-[200px] lg:w-[520px] xl:h-[200px] xl:w-[700px] h700:w-[150px] ipadRange:w-[570px] tabletRange:w-[190px]">
+            <div className="h700:w-[150px] ipadRange:w-[570px] tabletRange:w-[190px] flex h-[220px] w-[175px] flex-col items-center justify-between p-4 sm:h-[150px] sm:w-[300px] md:h-[150px] md:w-[500px] lg:h-[200px] lg:w-[520px] xl:h-[200px] xl:w-[700px]">
               {currentEvent ? (
                 <>
-                  <h2 className="mb-2 text-center text-md font-bold text-black sm:text-lg md:text-2xl lg:text-3xl tabletRange:text-sm">
+                  <h2 className="text-md tabletRange:text-sm mb-2 text-center font-bold text-black sm:text-lg md:text-2xl lg:text-3xl">
                     Current Event: {currentEvent.name}
                   </h2>
                   <p className="text-sm font-medium text-black sm:text-sm md:text-base lg:text-lg">
-                    {format(currentEvent.startDate, "h:mm a")} - {format(currentEvent.endDate, "h:mm a")}
+                    {format(currentEvent.startDate, "h:mm a")} -{" "}
+                    {format(currentEvent.endDate, "h:mm a")}
                   </p>
-                  <p className="text-sm text-black sm:text-sm md:text-base lg:text-lg">{currentEvent.description}</p>
-                  <p className="text-sm text-black font-semibold sm:text-sm md:text-base lg:text-lg">
+                  <p className="text-sm text-black sm:text-sm md:text-base lg:text-lg">
+                    {currentEvent.description}
+                  </p>
+                  <p className="text-sm font-semibold text-black sm:text-sm md:text-base lg:text-lg">
                     Location: {currentEvent.location}
                   </p>
                   <Image
@@ -387,17 +395,17 @@ const SchedulePage: React.FC = () => {
                 </>
               ) : (
                 <div>
-                    <h2 className="pt-4 text-center text-xs font-bold text-blue-500 sm:text-lg md:text-2xl lg:text-3xl tabletRange:text-sm">
-                      No event currently happening! <br/> 
+                  <h2 className="tabletRange:text-sm pt-4 text-center text-xs font-bold text-blue-500 sm:text-lg md:text-2xl lg:text-3xl">
+                    No event currently happening! <br />
                     Check the countdown for our next one :D
-                    </h2>
-                    <Image
-                      src="/Pixel_PolarBear.png"
-                      className="visible absolute -bottom-4 -right-5 tabletRange:w-[100px] "
-                      width={150}
-                      height={150}
-                      alt="polar bear"
-                    />
+                  </h2>
+                  <Image
+                    src="/Pixel_PolarBear.png"
+                    className="tabletRange:w-[100px] visible absolute -bottom-4 -right-5 "
+                    width={150}
+                    height={150}
+                    alt="polar bear"
+                  />
                 </div>
               )}
             </div>
@@ -409,16 +417,16 @@ const SchedulePage: React.FC = () => {
           onFocus={setFocusedWindow}
           name="events"
           focus={focusedWindow}
-          className="absolute right-4 tabletRange:right-[10px] top-[270px] sm:right-8 sm:top-[340px] md:right-[60px] md:top-[120px] ipadRange:top-[500px] ipadproRange:top-[620px] tabletRange:top-[150px]"
+          className="tabletRange:right-[10px] ipadRange:top-[500px] ipadproRange:top-[620px] tabletRange:top-[150px] absolute right-4 top-[270px] sm:right-8 sm:top-[340px] md:right-[60px] md:top-[120px]"
         >
           <WindowContainer
             isOpen={true}
-            openFunc={() => { }}
+            openFunc={() => {}}
             borderGradientStart="#34a4eb"
             borderGradientMiddle="#004c99"
             borderGradientEnd="#124c87"
           >
-            <div className="tabletRange:h-[300px] flex ipadproRange:w-[600px] h-[420px] w-[300px] flex-col items-center p-4 sm:h-[300px] sm:w-[300px] md:h-[450px] md:w-[400px] lg:h-[480px] lg:w-[420px] h700:h-[250px] ipadRange:h-[300px]">
+            <div className="tabletRange:h-[300px] ipadproRange:w-[600px] h700:h-[250px] ipadRange:h-[300px] flex h-[420px] w-[300px] flex-col items-center p-4 sm:h-[300px] sm:w-[300px] md:h-[450px] md:w-[400px] lg:h-[480px] lg:w-[420px]">
               <h2 className="mb-2 text-2xl font-semibold sm:mb-4 sm:text-3xl md:text-4xl lg:text-5xl">
                 Events!!!
               </h2>
@@ -435,18 +443,17 @@ const SchedulePage: React.FC = () => {
                   .map((event) => (
                     <div
                       key={event.id}
-                      className={`compStyling clickable-box w-full cursor-pointer rounded-lg border ${new Date() > event.endDate
+                      className={`compStyling clickable-box w-full cursor-pointer rounded-lg border ${
+                        new Date() > event.endDate
                           ? "bg-gray-200 text-gray-500 line-through" // Entire div with line-through for past events
                           : new Date() >= event.startDate &&
                               new Date() <= event.endDate
-                              ? "border-blue-500 font-bold text-blue-500"
-                              : "bg-[#f5f5f5] text-black"
-                        } hover:bg-[#e4e3e4]`}
+                            ? "border-blue-500 font-bold text-blue-500"
+                            : "bg-[#f5f5f5] text-black"
+                      } hover:bg-[#e4e3e4]`}
                       onClick={() => handleEventClick(event)}
                     >
-                      <h3
-                        className="text-sm font-semibold sm:text-base md:text-lg"
-                      >
+                      <h3 className="text-sm font-semibold sm:text-base md:text-lg">
                         {event.name}
                       </h3>
                       <p className="text-xs sm:text-sm">
@@ -459,9 +466,7 @@ const SchedulePage: React.FC = () => {
                     </div>
                   ))}
 
-
-
-                <h2 className="mt-4 mb-2 text-center text-lg font-bold sm:mb-3 sm:text-xl md:mb-4 md:text-2xl p-2 lg:text-3xl">
+                <h2 className="mb-2 mt-4 p-2 text-center text-lg font-bold sm:mb-3 sm:text-xl md:mb-4 md:text-2xl lg:text-3xl">
                   Sunday
                 </h2>
                 {events
@@ -469,12 +474,14 @@ const SchedulePage: React.FC = () => {
                   .map((event) => (
                     <div
                       key={event.id}
-                      className={`compStyling clickable-box w-full cursor-pointer rounded-lg border ${new Date() > event.endDate
+                      className={`compStyling clickable-box w-full cursor-pointer rounded-lg border ${
+                        new Date() > event.endDate
                           ? "bg-gray-200 text-gray-500"
-                          : new Date() >= event.startDate && new Date() <= event.endDate
-                            ? "border-blue-500 text-blue-500 font-bold"
+                          : new Date() >= event.startDate &&
+                              new Date() <= event.endDate
+                            ? "border-blue-500 font-bold text-blue-500"
                             : "bg-[#f5f5f5] text-black"
-                        } hover:bg-[#e4e3e4]`}
+                      } hover:bg-[#e4e3e4]`}
                       onClick={() => handleEventClick(event)}
                     >
                       <h3
@@ -483,7 +490,8 @@ const SchedulePage: React.FC = () => {
                         {event.name}
                       </h3>
                       <p className="text-xs sm:text-sm">
-                        {format(event.startDate, "MMM d, yyyy, h:mm a")} - {format(event.endDate, "h:mm a")}
+                        {format(event.startDate, "MMM d, yyyy, h:mm a")} -{" "}
+                        {format(event.endDate, "h:mm a")}
                       </p>
                     </div>
                   ))}
