8c9c365615dc7d0f4fcad148c06801d750cf1264
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-12-20 15:48:23 -0500

    Added BSD v3 license

---
+++ my-turborepo/packages/db/drizzle.config.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+if (!process.env.POSTGRES_URL) {
+    throw new Error("Missing POSTGRES_URL");
+}
+var nonPoolingUrl = process.env.POSTGRES_URL.replace(":6543", ":5432");
+exports.default = {
+    schema: "./src/schema.ts",
+    dialect: "postgresql",
+    dbCredentials: { url: nonPoolingUrl },
+    tablesFilter: ["t3turbo_*"],
+    out: "./drizzle",
+};
