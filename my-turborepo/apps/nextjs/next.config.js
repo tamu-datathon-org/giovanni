a5af8d50454584d07b0da4173a45ad438dd3ac01
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-12-29 15:20:14 -0500

    Added Dockerfile for nextjs applications

--- my-turborepo/apps/nextjs/next.config.js
+++ my-turborepo/apps/nextjs/next.config.js
@@ -1,8 +1,8 @@
-import { fileURLToPath } from "url";
-import createJiti from "jiti";
+// import { fileURLToPath } from "url";
+// import createJiti from "jiti";
 
 // Import env files to validate at build time. Use jiti so we can load .ts files in here.
-createJiti(fileURLToPath(import.meta.url))("./src/env");
+// createJiti(fileURLToPath(import.meta.url))("./src/env");
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -20,6 +20,7 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  output: "standalone",
 };
 
 export default config;
