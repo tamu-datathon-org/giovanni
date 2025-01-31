fcc8e81fb05923e0e54124534ce12028b182f3c7
hetk987 <66132321+hetk987@users.noreply.github.com>
2025-01-31 10:51:10 -0600

    Finished footer and about section

--- my-turborepo/apps/team-website/src/app/template-components/Hero/index.tsx
+++ my-turborepo/apps/team-website/src/app/template-components/Hero/index.tsx
@@ -12,15 +12,19 @@ const Hero = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Free and Open-Source Next.js Template for Startup & SaaS
+                  <span className="text-[#6EFEEB] ">tamu</span>
+                  <span className="text-[#2D69DF]">datathon</span>
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                  Startup is free Next.js template for startups and SaaS
-                  business websites comes with all the essential pages,
-                  components, and sections you need to launch a complete
-                  business website, built-with Next 13.x and Tailwind CSS.
+                  At TAMU Datathon, we're passionate about data science and
+                  machine learning. We bring together students of all levels for
+                  an intense 24-hour hackathon at Texas A&M University. Our
+                  goal? To inspire, educate, and empower the next generation of
+                  data scientists through real-world challenges and expert
+                  mentorship. Join us as we push the boundaries of data-driven
+                  innovation!
                 </p>
-                <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+                {/* <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="https://nextjstemplates.com/templates/saas-starter-startup"
                     className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
@@ -33,7 +37,7 @@ const Hero = () => {
                   >
                     Star on GitHub
                   </Link>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
