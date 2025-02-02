e09bda5829b6fced7c5a185ef3014e77a6d8029a
Betheony <betheony@tamu.edu>
2025-02-01 23:07:01 -0600

    fixed padding

--- my-turborepo/apps/team-website/src/app/template-components/Apply/ApplySection.tsx
+++ my-turborepo/apps/team-website/src/app/template-components/Apply/ApplySection.tsx
@@ -19,8 +19,8 @@ const ApplySection = () => {
         </Link>
       </div>
       <div className="container">
-        <div className="-mx-4 flex flex-wrap items-center">
-          <div className="w-full px-4 lg:w-1/2">
+        <div className="flex flex-wrap items-center">
+          <div className="w-full h-full px-4 lg:w-1/2">
             <div
               className="relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
               data-wow-delay=".15s"
@@ -36,13 +36,13 @@ const ApplySection = () => {
                 src="/images/about/Organizer Recruitment.png"
                 alt="about image"
                 width={500}
-                height={500}
+                height={900}
                 className="hidden drop-shadow-three dark:block dark:drop-shadow-none"
               />
             </div>
           </div>
           <div className="w-full px-4 lg:w-1/2">
-            <div className="max-w-[470px]">
+            <div className="w-full">
               <div className="mb-9">
                 <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
                   Logistics
