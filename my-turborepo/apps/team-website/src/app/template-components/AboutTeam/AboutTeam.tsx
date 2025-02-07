8d4fa914c40ac0ea23c71150b510cbef494248c1
aniketshirodkar <shirodkar.aniket@gmail.com>
2025-02-06 23:38:25 -0600

    grid format

--- my-turborepo/apps/team-website/src/app/template-components/AboutTeam/AboutTeam.tsx
+++ my-turborepo/apps/team-website/src/app/template-components/AboutTeam/AboutTeam.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import React from "react";
 import TeamMember from "./teamMember";
 
 const AboutTeam = () => {
-  const teamMembers = Array.from({ length: 16 }).map((_, index) => ({
+  const teamMembers = Array.from({ length: 24 }).map((_, index) => ({
     image: `/images/teampics/placeholder.jpg`,
     name: `Team Member ${index + 1}`,
     position: "Position",
@@ -14,7 +16,7 @@ const AboutTeam = () => {
         <h2 className="mb-8 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
           About the Team
         </h2>
-        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
           {teamMembers.map((member, index) => (
             <TeamMember
               key={index}
