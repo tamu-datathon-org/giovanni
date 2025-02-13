039c7a830b76779f259479470149e670e45dea27
redninjale <redninjale@gmail.com>
2025-02-13 00:55:51 -0600

    finished headshots (only missing cindy)

--- my-turborepo/apps/team-website/src/app/template-components/AboutTeam/AboutTeam.tsx
+++ my-turborepo/apps/team-website/src/app/template-components/AboutTeam/AboutTeam.tsx
@@ -8,8 +8,8 @@ const AboutTeam = () => {
   [
     {
       teamMembers: [
-        { image: "/images/teampics/dayo_headshot.jpg", name: "Dayo", position: "President" },
-        { image: "/images/teampics/jonathan_headshot.jpg", name: "Jonathan Herrara", position: "Vice-President" },
+        { image: "/images/teampics/dayo_headshot.jpg", name: "Oluwadayo Bamgbelu", position: "President" },
+        { image: "/images/teampics/jonathan_headshot.jpg", name: "Jonathan Herrera", position: "Vice-President" },
         { image: "/images/teampics/warren_headshot.jpg", name: "Warren Wu", position: "Former President" },
         { image: "/images/teampics/victoria_headshot.jpg", name: "Victoria Chen", position: "Former Vice-President" },
 
@@ -18,51 +18,52 @@ const AboutTeam = () => {
     },
     {
       teamMembers: [
-        { image: "/images/teampics/placeholder.jpg", name: "Rishabh", position: "Challenges Lead" },
-        { image: "/images/teampics/vikram_headshot.jpg", name: "Vikram", position: "Challenges" },
-        { image: "/images/teampics/anthony_headshot.jpg", name: "Anthony", position: "Challenges" },
-        { image: "/images/teampics/akil_headshot.jpg", name: "Akil", position: "Challenges" },
-        { image: "/images/teampics/caleb_headshot.jpg", name: "Caleb", position: "Challenges" },
-        { image: "/images/teampics/zeshawn_headshot.jpg", name: "Zeshawn", position: "Challenges" },
+        { image: "/images/teampics/rishab_headshot.jpg", name: "Rishab Jadhav", position: "Challenges Lead" },
+        { image: "/images/teampics/vikram_headshot.jpg", name: "Sreevikram Rajaraman", position: "Challenges" },
+        { image: "/images/teampics/caleb_headshot.jpg", name: "Caleb Xu", position: "Challenges" },
+        { image: "/images/teampics/akil_headshot.jpg", name: "Akil Manivannan", position: "Former Challenges Lead" },
+        { image: "/images/teampics/anthony_headshot.jpg", name: "Anthony Ciardelli", position: "Former Challenges" },
+        { image: "/images/teampics/zeshawn_headshot.jpg", name: "Zeeshan CHatur", position: "Former Challenges" },
       ],
       description: "Challenges",
     },
     {
       teamMembers: [
         { image: "/images/teampics/cameron_headshot.jpg", name: "Cameron Le", position: "Dev Lead" },
-        { image: "/images/teampics/samay_headshot.jpg", name: "Samay", position: "Former Dev Lead" },
-        { image: "/images/teampics/bethany_headshot.jpg", name: "Bethany", position: "Dev" },
-        { image: "/images/teampics/het_headshot.jpg", name: "Het", position: "Dev" },
-        { image: "/images/teampics/aniket_headshot.jpg", name: "Aniket", position: "Dev" }
+        { image: "/images/teampics/bethany_headshot.jpg", name: "Bethany Tran", position: "Dev" },
+        { image: "/images/teampics/het_headshot.jpg", name: "Het Koradia", position: "Dev" },
+        { image: "/images/teampics/aniket_headshot.jpg", name: "Aniket Shirodkar", position: "Dev" },
+        { image: "/images/teampics/samay_headshot.jpg", name: "Samay Upadhyay", position: "Former Dev Lead" },
       ],
       description: "Developers",
     },
     {
       teamMembers: [
-        { image: "/images/teampics/timothy_headshot.jpg", name: "Timothy", position: "Outreach Lead" },
-        { image: "/images/teampics/rance_headshot.jpg", name: "Rance", position: "Former Outreach" },
+        { image: "/images/teampics/timothy_headshot.jpg", name: "Timothy Tsai", position: "Outreach Lead" },
+        { image: "/images/teampics/daniel_headshot.jpg", name: "Daniel Vinnik", position: "Former Outreach Lead" },
+        { image: "/images/teampics/rance_headshot.jpg", name: "Rance Blancaflor", position: "Former Outreach" },
         { image: "/images/teampics/chris_headshot.jpg", name: "Chris Kong", position: "Former Outreach" },
-        { image: "/images/teampics/daniel_headshot.jpg", name: "Daniel", position: "Former Outreach lead" },
       ],
       description: "Outreach",
     },
     {
       teamMembers: [
-        { image: "/images/teampics/malika_headshot.jpg", name: "Malika", position: "Logistics Lead" },
-        { image: "/images/teampics/amber_headshot.jpg", name: "Amber Chang", position: "Former Logistics Lead" },
+        { image: "/images/teampics/malika_headshot.jpg", name: "Mallika Parajuli", position: "Logistics Lead" },
         { image: "/images/teampics/summer_headshot.jpg", name: "Summer Wong", position: "Logistics" },
-        { image: "/images/teampics/brittney_headshot.jpg", name: "Brittney", position: "Logistics" },
-        { image: "/images/teampics/placeholder.jpg", name: "Trey", position: "Logistics" },
-        { image: "/images/teampics/placeholder.jpg", name: "Josh Brittleman", position: "Former Logistics" },
+        { image: "/images/teampics/brittney_headshot.jpg", name: "Brittney Sanchez", position: "Logistics" },
+        { image: "/images/teampics/trey_headshot.jpg", name: "Trey Schaider", position: "Logistics" },
+        // { image: "/images/teampics/placeholder.jpg", name: "Cindy Lam", position: "Former Logistics" },
+        { image: "/images/teampics/amber_headshot.jpg", name: "Amber Chang", position: "Former Logistics Lead" },
+        { image: "/images/teampics/josh_headshot.jpg", name: "Josh Bittlestone", position: "Former Logistics" },
       ],
       description: "Logistics",
     },
     {
       teamMembers: [
-        { image: "/images/teampics/kelsie_headshot.jpg", name: "Kelsie", position: "Design Lead" },
-        { image: "/images/teampics/kendall_headshot.jpg", name: "Kendall", position: "Design" },
-        { image: "/images/teampics/linh_headshot.jpg", name: "Linh", position: "Former Design" },
-        { image: "/images/teampics/heather_headshot.jpg", name: "Heather", position: "Former Design" },
+        { image: "/images/teampics/kelsie_headshot.jpg", name: "Kelsie Sheridan", position: "Design Lead" },
+        { image: "/images/teampics/kendall_headshot.jpg", name: "Kendall Nguyen", position: "Design" },
+        { image: "/images/teampics/linh_headshot.jpg", name: "Lynn Nie", position: "Former Design" },
+        { image: "/images/teampics/heather_headshot.jpg", name: "Heather An", position: "Former Design" },
       ],
       description: "Designers",
     },
--- my-turborepo/apps/team-website/src/app/template-components/AboutTeam/teamMember.tsx
+++ my-turborepo/apps/team-website/src/app/template-components/AboutTeam/teamMember.tsx
@@ -9,14 +9,14 @@ export interface TeamMemberProps {
 
 const TeamMember: React.FC<TeamMemberProps> = ({ image, name, position }) => {
   return (
-    <div className="px-4">
+    <div className="">
       <div className="text-center w-40">
         <Image
           src={image}
           alt={name}
           width={100}
           height={100}
-          className="mx-auto mb-4 h-40 w-40 rounded-full object-cover"
+          className="mx-auto mb-4 h-30 w-30 rounded-full object-cover"
         />
         <h3 className="text-base font-semibold text-black dark:text-white">
           {name}
