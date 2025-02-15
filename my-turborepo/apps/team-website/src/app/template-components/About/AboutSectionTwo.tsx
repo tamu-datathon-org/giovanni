e0085811fa01f20d897d1e479fa0eeaaf2ff215f
Betheony <betheony@tamu.edu>
2025-02-14 18:41:49 -0600

    finished floatinf stats

--- my-turborepo/apps/team-website/src/app/template-components/About/AboutSectionTwo.tsx
+++ my-turborepo/apps/team-website/src/app/template-components/About/AboutSectionTwo.tsx
@@ -1,56 +1,24 @@
 import Image from "next/image";
+import FloatingStats from "./FloatingStats";
 
 const AboutSectionTwo = () => {
   return (
     <section className="py-16 md:py-20 lg:py-28">
-      <div className="container">
+      <div className="container">   
         <div className="-mx-4 flex flex-wrap items-center">
-          <div className="w-full px-4 lg:w-1/2">
-            <div
-              className="relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
-              data-wow-delay=".15s"
-            >
-              <Image
-                src="/images/about/about-image-2.svg"
-                alt="about image"
-                fill
-                className="drop-shadow-three dark:hidden dark:drop-shadow-none"
-              />
-              <Image
-                src="/images/about/about-image-2-dark.svg"
-                alt="about image"
-                fill
-                className="hidden drop-shadow-three dark:block dark:drop-shadow-none"
-              />
-            </div>
-          </div>
-          <div className="w-full px-4 lg:w-1/2">
-            <div className="max-w-[470px]">
+          <FloatingStats/>
+          <div className="w-full px-4 text-center lg:w-1/2">
+            <div className="max-w-[700px]">
               <div className="mb-9">
                 <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                  Bug free code
+                  Innovate Through Challenge
                 </h3>
                 <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                We believe in challenge-based problem solving instead of traditional themes. Participants gain industry-relevant skills by working on real-world challenges. Designed for both beginners and experienced individuals.
                 </p>
-              </div>
-              <div className="mb-9">
-                <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                  Premier support
-                </h3>
-                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt.
-                </p>
-              </div>
-              <div className="mb-1">
-                <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                  Next.js
-                </h3>
-                <p className="text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
-                  Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt
-                  consectetur adipiscing elit setim.
+
+                <p className="pt-6 text-base font-medium leading-relaxed text-body-color sm:text-lg sm:leading-relaxed">
+                TAMU Datathon offers workshops, mentorship, and resources to help participants enhance their data science and machine learning skills.
                 </p>
               </div>
             </div>
