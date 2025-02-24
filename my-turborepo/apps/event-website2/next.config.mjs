fbbba6fe00a58ceefde1a06f30238f626f987e11
redninjale <redninjale@gmail.com>
2025-02-24 00:07:45 -0600

    clean up

---
+++ my-turborepo/apps/event-website2/next.config.mjs
@@ -0,0 +1,28 @@
+import mdx from "@next/mdx";
+
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {},
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: [
+    "@vanni/api",
+    "@vanni/auth",
+    "@vanni/db",
+    "@vanni/ui",
+    "@vanni/validators",
+  ],
+
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  output: "standalone",
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+};
+
+export default withMDX(nextConfig);
