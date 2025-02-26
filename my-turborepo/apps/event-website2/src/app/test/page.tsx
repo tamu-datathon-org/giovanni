bcfbbf87a8636c13ceb4070ad9e2a38e94bdf93c
redninjale <redninjale@gmail.com>
2025-02-26 15:36:29 -0600

    testing

---
+++ my-turborepo/apps/event-website2/src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Test Page</h1>
+      <p>This is a simple page created with React and TypeScript.</p>
+    </div>
+  );
+};
+
+export default TestPage;
