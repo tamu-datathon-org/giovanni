a4e7f630b2dd50f8c121b64088aa10f418dc115d
redninjale <redninjale@gmail.com>
2025-02-27 20:46:12 -0600

    signout

--- my-turborepo/apps/nextjs/src/app/organizer/page.tsx
+++ my-turborepo/apps/nextjs/src/app/organizer/page.tsx
@@ -1,7 +1,15 @@
+import { signOut } from "@vanni/auth";
+
 export default function OrganizerPage() {
   return (
     <div>
       <h1>Welcome to the Organizer Page</h1>
+      <button onClick={ async () => {
+        "use server";
+        await signOut();
+      }}>
+        Sign Out!
+      </button>
     </div>
   );
 }
