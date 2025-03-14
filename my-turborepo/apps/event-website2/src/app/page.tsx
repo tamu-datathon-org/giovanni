54148c30422c12bfb938fb0a95851acd0e3e9b55
redninjale <redninjale@gmail.com>
2025-03-13 23:01:21 -0500

    added the landing-page

--- my-turborepo/apps/event-website2/src/app/page.tsx
+++ my-turborepo/apps/event-website2/src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { GetServerSideProps } from 'next';
 
-import { Heading, Flex, Text, Button, Avatar, RevealFx, Arrow, Column } from "@/once-ui/components";
+import { Heading, Flex, Text, Avatar, RevealFx, Arrow, Column } from "@/once-ui/components";
 import { Projects } from "@/components/work/Projects";
 
 import { baseURL, routes } from "@/app/resources";
@@ -10,6 +10,7 @@ import { Mailchimp } from "@/components";
 import { Posts } from "@/components/blog/Posts";
 import FAQComponent from "@/components/FAQ/FAQComponent";
 import TestTRPC from "@/components/test";
+import LandingPage from "@/components/LandingPage";
 
 export async function generateMetadata() {
   const title = home.title;
@@ -74,39 +75,10 @@ export default function Home() {
           }),
         }}
       />
-      <TestTRPC />
       <Column fillWidth paddingY="l" gap="m">
-        <Column maxWidth="s">
-          <RevealFx translateY="4" fillWidth horizontal="start" paddingBottom="m">
-            <Heading wrap="balance" variant="display-strong-l">
-              {home.headline}
-            </Heading>
-          </RevealFx>
-          <RevealFx translateY="8" delay={0.2} fillWidth horizontal="start" paddingBottom="m">
-            <Text wrap="balance" onBackground="neutral-weak" variant="heading-default-xl">
-              {home.subline}
-            </Text>
-          </RevealFx>
-          <RevealFx translateY="12" delay={0.4} horizontal="start">
-            <Button
-              id="about"
-              data-border="rounded"
-              href="/about"
-              variant="secondary"
-              size="m"
-              arrowIcon
-            >
-              <Flex gap="8" vertical="center">
-                {about.avatar.display && (
-                  <Avatar
-                    style={{ marginLeft: "-0.75rem", marginRight: "0.25rem" }}
-                    src={person.avatar}
-                    size="m"
-                  />
-                )}
-                {about.title}
-              </Flex>
-            </Button>
+        <Column fillWidth>
+          <RevealFx translateY="4" fillWidth horizontal="center" paddingBottom="m">
+            <LandingPage />
           </RevealFx>
         </Column>
       </Column>
