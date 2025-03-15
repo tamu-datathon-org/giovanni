41a0b4cf1465be19d71ffd65bbc16f4ea289299f
aniketshirodkar <shirodkar.aniket@gmail.com>
2025-03-14 23:17:15 -0500

    event info components

--- my-turborepo/apps/team-website/src/app/page.tsx
+++ my-turborepo/apps/team-website/src/app/page.tsx
@@ -1,5 +1,6 @@
 import AboutSectionOne from "@/app/template-components/About/AboutSectionOne";
 import ApplySection from "@/app/template-components/Apply/ApplySection";
+import EventInfo from "@/app/template-components/EventInfo/EventInfo";
 import Blog from "@/app/template-components/Blog";
 import Brands from "@/app/template-components/Brands";
 import ScrollUp from "@/app/template-components/Common/ScrollUp";
@@ -10,11 +11,16 @@ import Testimonials from "@/app/template-components/Testimonials";
 import Video from "@/app/template-components/Video";
 import { Metadata } from "next";
 import AboutTeam from "./template-components/AboutTeam/AboutTeam";
-import { Carousel, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import {
+  Carousel,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export const metadata: Metadata = {
   title: "TAMU Datathon",
-  description: "TAMU Datathon is a 24-hour hackathon hosted by Texas A&M University.",
+  description:
+    "TAMU Datathon is a 24-hour hackathon hosted by Texas A&M University.",
 };
 
 export default function Home() {
@@ -26,8 +32,9 @@ export default function Home() {
       {/* <Video /> */}
       {/* <Brands /> */}
       <AboutSectionOne />
-      <ApplySection/>
+      <ApplySection />
       <AboutTeam />
+      <EventInfo />
       {/* <Testimonials /> */}
       <Blog />
       <Contact />
