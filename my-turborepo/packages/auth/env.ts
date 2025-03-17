a9922894627e316000eafb6c8cd8f37b660d5e97
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2025-03-16 22:44:33 -0500

    Removed Discord auth provider

--- my-turborepo/packages/auth/env.ts
+++ my-turborepo/packages/auth/env.ts
@@ -4,8 +4,6 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    AUTH_DISCORD_ID: z.string().min(1),
-    AUTH_DISCORD_SECRET: z.string().min(1),
     AUTH_AUTH0_ID: z.string().min(1),
     AUTH_AUTH0_SECRET: z.string().min(1),
     AUTH_AUTH0_DOMAIN: z.string().url(),
