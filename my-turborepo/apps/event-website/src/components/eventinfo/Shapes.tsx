fd8b9a1646e88ccfbeb190a02c082f0c26401437
Betheony <betheony@tamu.edu>
2025-03-23 17:08:59 -0500

    location/event info

---
+++ my-turborepo/apps/event-website/src/components/eventinfo/Shapes.tsx
@@ -0,0 +1,25 @@
+export default function Shapes() {
+    return (
+        <svg width="272" height="87" viewBox="0 0 272 87" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_d_359_5)">
+                <circle cx="43.3293" cy="39.3293" r="34.3293" stroke="#36393E" stroke-width="10"/>
+                <path d="M105.818 73.2652L136.378 11.3036L166.938 73.2652H105.818Z" stroke="#36393E" stroke-width="10"/>
+                <rect x="194.866" y="5.52439" width="68.1341" height="68.1341" stroke="#36393E" stroke-width="10"/>
+            </g>
+            <defs>
+                <filter id="filter0_d_359_5" x="0" y="0" width="272" height="86.6585" filterUnits="userSpaceOnUse"
+                        color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                                   result="hardAlpha"/>
+                    <feOffset dy="4"/>
+                    <feGaussianBlur stdDeviation="2"/>
+                    <feComposite in2="hardAlpha" operator="out"/>
+                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_359_5"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_359_5" result="shape"/>
+                </filter>
+            </defs>
+        </svg>
+    );
+}
