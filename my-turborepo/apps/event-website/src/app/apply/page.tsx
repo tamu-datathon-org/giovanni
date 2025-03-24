c5b35ca4ba1a936c20c678427db33f854642f41a
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2025-03-24 15:30:43 -0500

    Updated redirect

--- my-turborepo/apps/event-website/src/app/apply/page.tsx
+++ my-turborepo/apps/event-website/src/app/apply/page.tsx
@@ -4,14 +4,6 @@ import React from 'react';
 
 
 // All this does is redirect people to tamudatathon.org/apply if they try to access tamudatathon.com/apply
-export default function ApplyLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-
-  // TODO: Maybe set this to a variable that updates according to the real site
+export default function ApplyPage() {
   redirect("https://tamudatathon.org/apply");
-  return (<>
-      {children}
-  </>
-  );
 }
