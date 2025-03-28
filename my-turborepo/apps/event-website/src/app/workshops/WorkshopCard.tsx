6d2e81677ddc36560ee66e1827a62d6f686b9747
redninjale <redninjale@gmail.com>
2025-03-28 15:48:55 -0500

    add lcoation + date

--- my-turborepo/apps/event-website/src/app/workshops/WorkshopCard.tsx
+++ my-turborepo/apps/event-website/src/app/workshops/WorkshopCard.tsx
@@ -4,13 +4,18 @@ import React from "react";
 type WorkshopCardProp = {
   title: string;
   description: string;
+  location: string;
+  date: string;
 }
 
-const WorkshopCard: React.FC<WorkshopCardProp> = ({ title, description }) => {
+const WorkshopCard: React.FC<{ workshop: WorkshopCardProp }> = ({ workshop }) => {
+  const { title, description, location, date } = workshop;
   return (
-    <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">{description}</p>
+    <div className="block max-w-sm p-6 border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+      <h1 className="text-2xl font-bold tracking-tight text-white">{title}</h1>
+      <span className="text-sm opacity-50">{date} | {location}</span>
+      <div className="h-4"></div>
+      <p className="font-normal text-white">{description}</p>
     </div>
   );
 };
@@ -20,23 +25,32 @@ const WorkshopSection: React.FC = () => {
   const workshops = [
     {
       title: "ETAM Workshop",
+      date: "March 27 7:00 - 8:00 PM",
+      location: "ILCB 206",
       description: "Focusing on ETAM applications, resunme buiding and intership prep along with headshots",
     },
     {
       title: "Python Beginner Workshop",
+      date: "April 1 7:00 - 8:00 PM",
+      location: "ILCB 206",
       description: "Learn skills to conquer challenges at TD Lite!",
     },
     {
       title: "Challenge Walkthrough",
+      date: "April 3 7:00 - 8:00 PM",
+      location: "ILCB 206",
       description: "We'll teach you the steps taking on a challenge aon with takin on NEW chaenge ourselves",
     },
   ];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-6">
-      {workshops.map((workshop, index) => (
-        <WorkshopCard key={index} title={workshop.title} description={workshop.description} />
-      ))}
+    <div>
+      <h1 className="w-full text-center text-3xl sm:text-4xl font-[myfont]">Workshops</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-6">
+        {workshops.map((workshop, index) => (
+          <WorkshopCard workshop={workshop}/>
+        ))}
+      </div>
     </div>
   );
 };
