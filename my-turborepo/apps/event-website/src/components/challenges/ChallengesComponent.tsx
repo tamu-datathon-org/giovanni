57949a71ba2b0803dbca31e2cb85b1a72b62b927
Betheony <betheony@tamu.edu>
2025-03-28 14:22:34 -0500

    added challenges component and piggy bank

---
+++ my-turborepo/apps/event-website/src/components/challenges/ChallengesComponent.tsx
@@ -0,0 +1,9 @@
+import ChallengesTitle from './ChallengesTitle';
+
+export default function ChallengesComponent() {
+    return (
+        <div id="Challenges" className="flex flex-col items-center">
+            <ChallengesTitle/>
+        </div>
+    );
+}
