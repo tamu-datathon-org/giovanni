6868275ca2649b88c5723cf8973c001109505462
Betheony <betheony@tamu.edu>
2025-03-28 21:31:26 -0500

    Cards added

--- my-turborepo/apps/event-website/src/components/challenges/ChallengesComponent.tsx
+++ my-turborepo/apps/event-website/src/components/challenges/ChallengesComponent.tsx
@@ -1,9 +1,18 @@
 import ChallengesTitle from './ChallengesTitle';
+import ChallengeCard from "@/components/challenges/ChallengeCard";
 
 export default function ChallengesComponent() {
     return (
-        <div id="Challenges" className="flex flex-col items-center">
+        <div id="Challenges" className="flex flex-col items-center gap-4">
             <ChallengesTitle/>
+            <div className="grid grid-cols-2 gap-4">
+                <ChallengeCard title="Game Recruitment" desc="Data manipulation"/>
+                <ChallengeCard title="Voice Command Survival" desc="Data manipulation"/>
+                <ChallengeCard title="Ddjakji" desc="Data manipulation"/>
+                <ChallengeCard title="Rev's Marbles" desc="Data manipulation"/>
+                <ChallengeCard title="Red Light Green Light" desc="Data manipulation"/>
+                <ChallengeCard title="Cookie's Regression" desc="Data manipulation"/>
+            </div>
         </div>
     );
 }
