ea692334016919da7132c05e0ef673cc3183c8b3
redninjale <redninjale@gmail.com>
2025-09-16 15:24:59 -0500

    Clear all the event website data

--- my-turborepo/apps/event-website/src/app/apply/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-// All this does is redirect people to tamudatathon.org/apply if they try to access tamudatathon.com/apply
-export default function ApplyPage() {
-  redirect("https://tamudatathon.org/apply");
-}
--- my-turborepo/apps/event-website/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { Header } from "@/components/Header";
-export { Footer } from "@/components/Footer";
-export { HeadingLink } from "@/components/HeadingLink";
-export { RouteGuard } from "@/components/RouteGuard";
