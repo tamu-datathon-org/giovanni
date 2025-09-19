0832b9f78b6609da9aeb90526d3750f750298f0a
redninjale <redninjale@gmail.com>
2025-09-19 11:33:12 -0500

    added mlh tag

--- my-turborepo/apps/event-website/src/app/layout.tsx
+++ my-turborepo/apps/event-website/src/app/layout.tsx
@@ -12,6 +12,27 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body>
+        <a
+          id="mlh-trust-badge"
+          style={{
+            display: "block",
+            maxWidth: "100px",
+            minWidth: "60px",
+            position: "fixed",
+            right: "50px",
+            top: "0",
+            width: "10%",
+            zIndex: 10000,
+          }}
+          href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=white"
+          target="_blank"
+        >
+          <img
+            src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-white.svg"
+            alt="Major League Hacking 2026 Hackathon Season"
+            style={{ width: "100%" }}
+          />
+        </a>
         {children}
         <Footer />
       </body>
