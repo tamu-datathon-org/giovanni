f46de2df245c27ef5d223cb52ec3f67f152d2bfa
LaylaASerrano <lserrano03@tamu.edu>
2025-09-18 23:12:42 -0500

    fixed

--- my-turborepo/apps/event-website/src/app/page.tsx
+++ my-turborepo/apps/event-website/src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
     <div className="flex flex-col min-h-screen w-full items-center justify-center">
       <div
         className="absolute inset-0 bg-no-repeat bg-center bg-contain
-                   bg-[url('/images/landing-page/LandingPage-mobile.png')]
+                   bg-[url('/images/landing-page/Landing-Page-phone.png')]
                    sm:bg-[url('/images/landing-page/LandingPage.png')]
                    sm:bg-cover sm:bg-center"
       />
