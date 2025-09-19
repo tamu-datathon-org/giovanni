360eafb7c23c2995479ec593622090e0be58d061
LaylaASerrano <lserrano03@tamu.edu>
2025-09-18 22:38:31 -0500

    added mobil scale down -issues with scalling for half screen

--- my-turborepo/apps/event-website/src/app/page.tsx
+++ my-turborepo/apps/event-website/src/app/page.tsx
@@ -1,26 +1,42 @@
-import { Button } from '@vanni/ui/button';
+"use client";
+
+import Footer from "@/components/Footer";
+import { useRouter } from 'next/navigation';
+import { ApplyButton } from "@/components/ui/apply-buttom";
+
 
 export default function Home() {
+  const router = useRouter()
+
+  const handleApplyClick = () => {
+    // Redirect to team website application page
+    window.open('https://tamudatathon.org/apply', '_blank')
+    // Or use Next.js router for internal navigation:
+    // router.push('/apply')
+  }
 
   return (
-    <div
-      className="flex flex-col min-h-screen w-full"
-      style={{
-        backgroundImage: "url('/images/landing-page/LandingPage.png')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-      }}
-    >
-      <div className="flex-1 relative">
+    <div className="flex flex-col min-h-screen w-full items-center justify-center">
+      <div
+        className="absolute inset-0 bg-no-repeat bg-center bg-contain
+                   bg-[url('/images/landing-page/LandingPage-mobile.png')]
+                   sm:bg-[url('/images/landing-page/LandingPage.png')]
+                   sm:bg-cover sm:bg-center"
+    />
+
+
+
+      <div className="flex-1 absolute w- h-80 bottom-20 mb-16 ">
         <a
           href="https://tamudatathon.org/apply"
           target="_blank"
           rel="noopener noreferrer"
+
         >
-          <Button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-            Apply Now
-          </Button>
+          <p className="text-6xl font-semi text-foreground mb-8 font-['KoPub_Batang']">APPLICATIONS CLOSE OCT. 24</p>
+          <div className="flex justify-center h-[100px]">
+            <ApplyButton />
+          </div>
         </a>
       </div>
     </div>
