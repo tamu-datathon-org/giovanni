2e811b9b4caf4d18c0df75f8831ddb1da917f6d6
LaylaASerrano <lserrano03@tamu.edu>
2025-10-07 21:14:56 -0500

    just the two cards

--- my-turborepo/apps/event-website/src/components/Hero/index.tsx
+++ my-turborepo/apps/event-website/src/components/Hero/index.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
         <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-[#1C0808]" />
 
         {/* light glow element */}
-        <div className="absolute -top-1/2 -left-1/2 w-[957px] h-[913px] opacity-60 bg-blend-overlay bg-[#FFF2AA] rounded-full blur-[350px]" />
+        {/* <div className="absolute -top-1/2 -left-1/2 w-[957px] h-[913px] opacity-60 bg-blend-overlay bg-[#FFF2AA] rounded-full blur-[350px]" /> */}
 
         {/*Newspaper section - centered in grid*/}
         <div className="relative z-10 col-start-2 col-span-10 row-start-3 row-span-8 flex items-top justify-center ">
@@ -40,7 +40,15 @@ const Hero = () => {
                 className="object-contain"
             />
         </div>
-        
+        <div className="relative z-20 col-start-1 col-span-2 row-start-1 row-span-2 w-full aspect-square ">
+            <Image
+                src="/images/elements/lamp.svg"
+                alt="mag"
+                width={600}
+                height={600}
+                className="object-contain"
+            />
+        </div>
     </div>
   );
 };
--- my-turborepo/apps/event-website/src/components/location/index.tsx
+++ my-turborepo/apps/event-website/src/components/location/index.tsx
@@ -4,15 +4,23 @@ import Link from "next/link";
 
 const Location = () => {
     return (
-        <div className="relative grid grid-cols-12 grid-rows-12 min-h-screen w-full overflow-hidden">
-            <Image
-                src="/images/logo/event-logo.svg"
-                alt="logo"
-                height={200}
-                width={200}
-                className="h-16 w-auto shrink-0 drop-shadow"
-                priority
-            />
+        <div className="relative grid grid-cols-12 grid-rows-12 min-h-screen w-full overflow-hidden pb-8">
+            <div className="absolute inset-0 bg-[#2A2523] bg-cover bg-center bg-no-repeat" />
+
+            {/* Brown on top, grey on bottom */}
+            <div className="absolute inset-0 bg-gradient-to-b from-[#1C0808] via-transparent to-transparent" />
+
+            <div className="relative z-10 col-start-2 col-span-10 row-start-3 row-span-8 flex items-center justify-center">
+                <div className="relative w-full h-full">
+                    <Image
+                        src="/images/elements/locationcard.svg"
+                        alt="location folder"
+                        fill
+                        className="object-contain"
+                        priority
+                    />
+                </div>
+            </div>
         </div>
    );
 };
