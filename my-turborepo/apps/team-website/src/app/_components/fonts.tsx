775e60c695e6bbf8cfc68925c59919256724faa2
hetk987 <66132321+hetk987@users.noreply.github.com>
2026-01-08 14:27:47 -0600

    Moving things to left and right

--- my-turborepo/apps/team-website/src/app/_components/fonts.tsx
+++
@@ -1,7 +0,0 @@
-import localFont from "next/font/local";
-
-export const w95fa = localFont({
-  src: "./fonts/w95fa.woff2",
-  display: "swap",
-  variable: "--font-w95fa",
-});
--- my-turborepo/apps/team-website/src/app/_components/organizer/schema.tsx
+++
@@ -1,6 +0,0 @@
-import type { Application } from "@vanni/db/schema";
-
-export type TableData = typeof Application.$inferSelect & {
-  resumeName: string | undefined;
-  resumeUrl: string | undefined;
-};
