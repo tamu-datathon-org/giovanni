fb394a6de26ade35508902038de744f7f33aee28
hetk987 <66132321+hetk987@users.noreply.github.com>
2026-01-16 13:29:10 -0600

    Test 2

--- my-turborepo/apps/team-website/src/components/Header/index.tsx
+++ my-turborepo/apps/team-website/src/components/Header/index.tsx
@@ -155,7 +155,6 @@ const Header = () => {
                                     </Link>
                                   ),
                                 )}
-                                
                               </div>
                             )}
                           </>
