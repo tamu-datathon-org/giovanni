1de986eac42ed7f08a6e5d5c9acda90c80f5cc16
hetk987 <66132321+hetk987@users.noreply.github.com>
2026-03-16 20:02:03 -0500

    merged

--- my-turborepo/apps/datathon-blog/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "../styles/global.css";
-
-import { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
