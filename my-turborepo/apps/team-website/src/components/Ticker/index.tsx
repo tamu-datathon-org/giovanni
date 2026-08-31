d5f9cc0cc7675cfbf2bfedffed26aa039d9b5b95
Zayd Nadir <zaydn4321@gmail.com>
2026-08-30 21:17:40 -0500

    Rebuild navbar as blue sidebar, add dots rail and loading screen

--- my-turborepo/apps/team-website/src/components/Ticker/index.tsx
+++ my-turborepo/apps/team-website/src/components/Ticker/index.tsx
@@ -101,7 +101,10 @@ export default function SponsorTicker() {
   }, []);
 
   return (
-    <div className="flex w-full flex-col items-center justify-center bg-[#121723] cursor-pointer">
+    <div
+      id="sponsors"
+      className="flex w-full cursor-pointer flex-col items-center justify-center bg-[#121723]"
+    >
       <SectionTitle title="Past Sponsors" paragraph={""} center mb="-40px" />
 
       <div
